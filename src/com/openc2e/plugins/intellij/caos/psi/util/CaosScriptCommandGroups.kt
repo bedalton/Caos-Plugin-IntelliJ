6d6146379e3a5534e6339b00b27ad922ec2e8df3
daniel <dsbadal@gmail.com>
2020-01-11 14:21:44 -0800

    Update CaosScriptCommandGroups.kt

--- src/com/openc2e/plugins/intellij/caos/psi/util/CaosScriptCommandGroups.kt
+++ src/com/openc2e/plugins/intellij/caos/psi/util/CaosScriptCommandGroups.kt
@@ -1,8 +1,8 @@
 package com.openc2e.plugins.intellij.caos.psi.util
 
-import com.intellij.psi.PsiElement
 import com.openc2e.plugins.intellij.caos.def.psi.api.CaosDefCommandDefElement
 import com.openc2e.plugins.intellij.caos.def.stubs.impl.CaosDefParameterStruct
+import com.openc2e.plugins.intellij.caos.psi.api.CaosScriptExpression
 import com.openc2e.plugins.intellij.caos.psi.api.CaosScriptExpressionList
 import com.openc2e.plugins.intellij.caos.utils.orElse
 import java.security.InvalidParameterException
@@ -17,12 +17,8 @@ class CaosScriptCommandGroups {
         var currentItem: CaosScriptItem? = null
         var i = 0
         var count = expressions.size
-        var skip = 0
+        var skip = listOf<List<String>>()
         for (expression in expressions) {
-            if (skip > 0) {
-                skip--
-                continue
-            }
             while (currentItem != null && !currentItem.needsMore()) {
                 stack.removeAt(0)
                 currentItem = stack.getOrNull(0)
@@ -31,6 +27,18 @@ class CaosScriptCommandGroups {
             var parameters: List<CaosDefParameterStruct>? = null
 
             if (commandToken != null) {
+                if (skip.isNotEmpty()) {
+                    val command = commandToken.commandString
+                    skip = skip.filter { it.getOrNull(0) == command }
+                    if (skip.isNotEmpty()) {
+                        skip = skip
+                                .filter { it.size > 1 }
+                                .map {
+                                    it.subList(1, it.lastIndex)
+                                }
+                        continue
+                    }
+                }
                 val inCommand = currentItem?.consume.orElse(0) > 0
                 var commandMatches = commandToken.reference
                         .multiResolve(true)
@@ -49,6 +57,16 @@ class CaosScriptCommandGroups {
                     if (variables.isNotEmpty())
                         commandMatches = variables
                 }
+                if (commandMatches.isNotEmpty()) {
+                    skip = commandMatches
+                            .map {
+                                it.commandWords
+                            }.filter { it.size > 1 }
+                            .map {
+                                it.subList(1, it.lastIndex)
+                            }
+                }
+
                 val parameterStructs = commandMatches.map { it.parameterStructs }
                 val min = parameterStructs.map { it.size }.min().orElse(0)
                 parameters = (0 until min).map { i ->
@@ -56,16 +74,23 @@ class CaosScriptCommandGroups {
                     set.first()
                 }
             }
-            val item =
+            var item:CaosScriptItem? = null
             if (currentItem != null) {
-
+                if (currentItem.needsMore()) {
+                    val parameter = currentItem.currentParam
+                    item = CaosScriptItem(expression, parameters, parameter)
+                }
             }
+            if (item == null)
+                item = CaosScriptItem(expression, parameters, parameter)
+
 
         }
     }
 }
 
-data class CaosScriptItem(val element: PsiElement, val parameters: List<CaosDefParameterStruct>?, var consumed: Int) {
+data class CaosScriptItem(val element: CaosScriptExpression, val parameters: List<CaosDefParameterStruct>?, val paramInfo:CaosDefParameterStruct? = null) {
+    private var consumed: Int = 0
     internal val consume: Int = parameters?.size.orElse(0)
     private val childElements = mutableListOf<CaosScriptItem>()
     val children: List<CaosScriptItem> get() = childElements
