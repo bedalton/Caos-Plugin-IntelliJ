773fbd2696d2dfda3c90806db16c4bc4be23bdee
daniel <dsbadal@gmail.com>
2020-05-18 10:23:56 -0700

    update indices

--- src/com/openc2e/plugins/intellij/caos/indices/CaosScriptIndexService.kt
+++ src/com/openc2e/plugins/intellij/caos/indices/CaosScriptIndexService.kt
@@ -1,8 +1,12 @@
 package com.openc2e.plugins.intellij.caos.indices
 
 import com.intellij.psi.stubs.IndexSink
-import com.openc2e.plugins.intellij.caos.stubs.api.CaosScriptSubroutineStub
+import com.openc2e.plugins.intellij.caos.stubs.api.*
 
 interface CaosScriptIndexService {
     fun indexSubroutine(subroutineStub: CaosScriptSubroutineStub, indexSink: IndexSink)
+    fun indexNamedVar(stub: CaosScriptNamedVarStub, indexSink: IndexSink)
+    fun indexConstantAssignment(stub: CaosScriptConstantAssignmentStub, indexSink: IndexSink)
+    fun indexNamedConstant(stub: CaosScriptNamedConstantStub, indexSink: IndexSink)
+    fun indexVarAssignment(stub: CaosScriptAssignmentStub, indexSink: IndexSink)
 }
