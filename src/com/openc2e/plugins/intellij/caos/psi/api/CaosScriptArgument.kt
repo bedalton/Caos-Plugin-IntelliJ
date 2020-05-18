d025d219bfe3b89629c521c6426630c9b0eaa06e
daniel <dsbadal@gmail.com>
2020-05-18 10:30:50 -0700

    Update CaosScriptArgument.kt

--- src/com/openc2e/plugins/intellij/caos/psi/api/CaosScriptArgument.kt
+++ src/com/openc2e/plugins/intellij/caos/psi/api/CaosScriptArgument.kt
@@ -2,8 +2,23 @@ package com.openc2e.plugins.intellij.caos.psi.api
 
 import com.intellij.psi.PsiElement
 import com.openc2e.plugins.intellij.caos.deducer.CaosScope
+import com.openc2e.plugins.intellij.caos.deducer.CaosVar
 
 interface CaosScriptArgument : CaosScriptCompositeElement {
     val index:Int
-    val scopes:List<CaosScope>
+    val expectedType:CaosScriptExpectedType
+    fun toCaosVar(): CaosVar
+}
+
+enum class CaosScriptExpectedType(val value:Int) {
+    INT(1),
+    FLOAT(2),
+    BYTE_STRING(3),
+    TOKEN(4),
+    AGENT(5),
+    DECIMAL(7),
+    C1_STRING(8),
+    STRING(9),
+    ANY(10),
+    VARIABLE(11)
 }
