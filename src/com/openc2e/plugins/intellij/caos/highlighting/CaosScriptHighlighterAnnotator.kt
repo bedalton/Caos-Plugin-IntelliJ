6b81f2bfac49ebb8ef93805707039b6c5058df88
daniel <dsbadal@gmail.com>
2020-05-18 10:23:36 -0700

    update command tokens

--- src/com/openc2e/plugins/intellij/caos/highlighting/CaosScriptHighlighterAnnotator.kt
+++ src/com/openc2e/plugins/intellij/caos/highlighting/CaosScriptHighlighterAnnotator.kt
@@ -6,21 +6,27 @@ import com.intellij.openapi.editor.colors.TextAttributesKey
 import com.intellij.openapi.editor.markup.TextAttributes
 import com.intellij.psi.PsiElement
 import com.openc2e.plugins.intellij.caos.annotators.AnnotationHolderWrapper
+import com.openc2e.plugins.intellij.caos.def.indices.CaosDefCommandElementsByNameIndex
 import com.openc2e.plugins.intellij.caos.psi.api.*
+import com.openc2e.plugins.intellij.caos.psi.impl.containingCaosFile
 import com.openc2e.plugins.intellij.caos.utils.hasParentOfType
+import com.openc2e.plugins.intellij.caos.utils.isOrHasParentOfType
 
 class CaosScriptHighlighterAnnotator : Annotator {
 
 
     override fun annotate(element: PsiElement, annotationHolder: AnnotationHolder) {
         val wrapper = AnnotationHolderWrapper(annotationHolder)
+        if (element !is CaosScriptCompositeElement)
+            return
         when {
             element is CaosScriptToken -> colorize(element, wrapper, CaosScriptSyntaxHighlighter.TOKEN)
             element is CaosScriptIsRvalueKeywordToken || element.parent is CaosScriptIsRvalueKeywordToken-> colorize(element, wrapper, CaosScriptSyntaxHighlighter.RVALUE_TOKEN)
             element is CaosScriptIsLvalueKeywordToken || element.parent is CaosScriptIsLvalueKeywordToken -> colorize(element, wrapper, CaosScriptSyntaxHighlighter.LVALUE_TOKEN)
-            isCommandKeyword(element) && element.text.toLowerCase() != "retn" -> colorize(element, wrapper, CaosScriptSyntaxHighlighter.COMMAND_TOKEN)
-            element is CaosScriptAnimationString || element.hasParentOfType(CaosScriptAnimationString::class.java) -> colorize(element, wrapper, CaosScriptSyntaxHighlighter.ANIMATION)
-            element is CaosScriptCGsub -> colorize(element.kGsub, wrapper, CaosScriptSyntaxHighlighter.KEYWORDS)
+            element is CaosScriptAnimationString -> colorize(element, wrapper, CaosScriptSyntaxHighlighter.ANIMATION)
+            element is CaosScriptByteString && isAnimationByteString(element) -> colorize(element, wrapper, CaosScriptSyntaxHighlighter.ANIMATION)
+            //isCommandKeyword(element) && element.text.toLowerCase() != "retn" -> colorize(element, wrapper, CaosScriptSyntaxHighlighter.COMMAND_TOKEN)
+            element is CaosScriptCGsub -> colorize(element.cKwGsub, wrapper, CaosScriptSyntaxHighlighter.KEYWORDS)
             element is CaosScriptSubroutineName -> colorize(element, wrapper, CaosScriptSyntaxHighlighter.SUBROUTINE_NAME)
             element.text.toLowerCase() == "inst" -> colorize(element, wrapper, CaosScriptSyntaxHighlighter.KEYWORDS)
         }
@@ -49,5 +55,29 @@ class CaosScriptHighlighterAnnotator : Annotator {
                 .range(psiElement)
                 .textAttributes(attribute)
                 .create()
+    }
+
+    private fun isAnimationByteString(element: CaosScriptByteString) : Boolean {
+        if (!element.hasParentOfType(CaosScriptExpectsByteString::class.java))
+            return false
+        val argumentParent = element.getParentOfType(CaosScriptArgument::class.java)
+                ?: return false
+        val index = argumentParent.index
+        val commandParent = element.getParentOfType(CaosScriptCommandElement::class.java)
+                ?: return false
+        val command = commandParent.commandString
+        val variant = element.containingCaosFile?.variant
+        return CaosDefCommandElementsByNameIndex.Instance[command, element.project]
+                .any {
+                    if (!it.isRvalue)
+                        return@any false
+                    if (variant != null && !it.isVariant(variant))
+                        return@any false
+                    val parameter = it.parameterList
+                            .getOrNull(index)
+                            ?.parameterType
+                            ?: return@any false
+                    parameter == "[anim]"
+                }
     }
 }
