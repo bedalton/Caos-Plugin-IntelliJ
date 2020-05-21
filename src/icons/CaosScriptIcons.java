c9a81c09b6259add0d2d318035919f0fb860ed65
daniel <dsbadal@gmail.com>
2020-05-21 10:48:49 -0700

    add icons

---
+++ src/icons/CaosScriptIcons.java
@@ -0,0 +1,16 @@
+package icons;
+
+import com.intellij.openapi.util.IconLoader;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+public interface CaosScriptIcons {
+    Icon SDK_ICON = IconLoader.getIcon("/icons/sdk-icon.png");
+    Icon CAOS_FILE_ICON = IconLoader.getIcon("/icons/caos-script-file-icon.png");
+    Icon CAOS_DEF_FILE_ICON = IconLoader.getIcon("/icons/caos-def-file-icon.png");
+    Icon COMMAND = IconLoader.getIcon("/icons/command-icon.png");
+    Icon RVALUE = IconLoader.getIcon("/icons/right-value-icon.png");
+    Icon LVALUE = IconLoader.getIcon("/icons/left-value-icon.png");
+    Icon HASHTAG = IconLoader.getIcon("/icons/hashtag-icon.png");
+}
