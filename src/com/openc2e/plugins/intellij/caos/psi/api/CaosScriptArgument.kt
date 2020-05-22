32278dca6fff9c8af924b3ca3206b57ae37babb9
daniel <dsbadal@gmail.com>
2020-05-22 13:00:46 -0700

    add type inspection

--- src/com/openc2e/plugins/intellij/caos/psi/api/CaosScriptArgument.kt
+++ src/com/openc2e/plugins/intellij/caos/psi/api/CaosScriptArgument.kt
@@ -1,25 +1,34 @@
 package com.openc2e.plugins.intellij.caos.psi.api
 
-import com.intellij.psi.PsiElement
-import com.openc2e.plugins.intellij.caos.deducer.CaosScope
 import com.openc2e.plugins.intellij.caos.deducer.CaosVar
 
 interface CaosScriptArgument : CaosScriptCompositeElement {
     val index:Int
-    val expectedType:CaosScriptExpectedType
+    val expectedType:CaosExpressionValueType
     fun toCaosVar(): CaosVar
 }
 
-enum class CaosScriptExpectedType(val value:Int) {
-    INT(1),
-    FLOAT(2),
-    BYTE_STRING(3),
-    TOKEN(4),
-    AGENT(5),
-    DECIMAL(7),
-    C1_STRING(8),
-    STRING(9),
-    ANY(10),
-    VARIABLE(11),
-    ANIMATION(12)
+enum class CaosExpressionValueType(val value:Int, val simpleName:String) {
+    INT(1, "integer"),
+    FLOAT(2, "float"),
+    TOKEN(3, "token"),
+    STRING(4, "string"),
+    VARIABLE(6, "variable"),
+    COMMAND(8, "command"),
+    C1_STRING(9, "[string]"),
+    BYTE_STRING(10, "[byte-string]"),
+    AGENT(11, "agent"),
+    ANY(13, "any"),
+    CONDITION(15, "condition"),
+    DECIMAL(16, "decimal"),
+    ANIMATION(17, "[anim]"),
+    HEXADECIMAL(18, "hexadecimal"),
+    NULL(0, "NULL"),
+    UNKNOWN(-1, "UNKNOWN");
+
+    companion object {
+        fun fromSimpleName(simpleName: String) : CaosExpressionValueType {
+            return values().firstOrNull { it.simpleName == simpleName } ?: UNKNOWN
+        }
+    }
 }
