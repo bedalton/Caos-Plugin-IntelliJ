ec8a1a78181ff4ead3e8c364375799b4b6bd7b95
bedalton <Dsbadal@gmail.com>
2020-05-24 07:22:44 -0700

    update typedef

--- src/com/openc2e/plugins/intellij/caos/def/indices/CaosDefStubIndexServiceImpl.kt
+++ src/com/openc2e/plugins/intellij/caos/def/indices/CaosDefStubIndexServiceImpl.kt
@@ -4,15 +4,12 @@ import com.intellij.psi.stubs.IndexSink
 import com.openc2e.plugins.intellij.caos.def.stubs.api.CaosDefCommandDefinitionStub
 import com.openc2e.plugins.intellij.caos.def.stubs.api.CaosDefDocCommentHashtagStub
 import com.openc2e.plugins.intellij.caos.def.stubs.api.CaosDefTypeDefinitionStub
-import com.openc2e.plugins.intellij.caos.utils.isNotNullOrBlank
 
 class CaosDefStubIndexServiceImpl : CaosDefStubIndexService {
 
 
     override fun indexCommand(stub: CaosDefCommandDefinitionStub, indexSink: IndexSink) {
         indexSink.occurrence(CaosDefCommandElementsByNameIndex.KEY, stub.command)
-        if (stub.namespace.isNotNullOrBlank())
-            indexSink.occurrence(CaosDefCommandElementsByNamespaceIndex.KEY, stub.namespace!!)
     }
 
     override fun indexTypeDef(stub: CaosDefTypeDefinitionStub, indexSink: IndexSink) {
