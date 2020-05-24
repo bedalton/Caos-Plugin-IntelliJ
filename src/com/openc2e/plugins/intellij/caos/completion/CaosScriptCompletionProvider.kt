37d8bcbca3eebecd1924c399ead473b30bc859be
daniel <dsbadal@gmail.com>
2020-05-24 13:12:33 -0700

    refactor code

--- src/com/openc2e/plugins/intellij/caos/completion/CaosScriptCompletionProvider.kt
+++ src/com/openc2e/plugins/intellij/caos/completion/CaosScriptCompletionProvider.kt
@@ -9,33 +9,25 @@ import com.intellij.psi.PsiElement
 import com.intellij.util.ProcessingContext
 import com.openc2e.plugins.intellij.caos.def.indices.CaosDefCommandElementsByNameIndex
 import com.openc2e.plugins.intellij.caos.lang.CaosScriptFile
-import icons.CaosScriptIcons
 import com.openc2e.plugins.intellij.caos.psi.api.CaosScriptCommandCall
 import com.openc2e.plugins.intellij.caos.psi.api.CaosScriptIncomplete
 import com.openc2e.plugins.intellij.caos.psi.api.CaosScriptLvalue
 import com.openc2e.plugins.intellij.caos.psi.api.CaosScriptRvalue
 import com.openc2e.plugins.intellij.caos.psi.util.*
-import com.openc2e.plugins.intellij.caos.psi.util.LOGGER
+import com.openc2e.plugins.intellij.caos.utils.Case
 import com.openc2e.plugins.intellij.caos.utils.isNotNullOrBlank
+import icons.CaosScriptIcons
 
 object CaosScriptCompletionProvider : CompletionProvider<CompletionParameters>() {
-
-    private val UPPERCASE_REGEX = "[A-Z]".toRegex()
-    private val IS_ID_CHAR = "[^_A-Za-z]".toRegex()
     private val IS_NUMBER = "[0-9]+".toRegex()
     private val WHITESPACE = "\\s+".toRegex()
-
+    private val SKIP_VAR_NAMES = listOf("VARX", "OBVX", "MVXX", "OVXX", "VAXX")
     override fun addCompletions(parameters: CompletionParameters, context: ProcessingContext, resultSet: CompletionResultSet) {
         val element = parameters.position
         val caosFile = element.containingFile.originalFile as? CaosScriptFile
                 ?: return
         val variant = caosFile.variant.toUpperCase()
-        val firstChar = element.textWithoutCompletionIdString.toCharArray().getOrNull(0)
-                ?: return
-        val case = if (firstChar == firstChar.toUpperCase())
-            Case.UPPER_CASE
-        else
-            Case.LOWER_CASE
+
         val text = element.textWithoutCompletionIdString
         if (IS_NUMBER.matches(text)) {
             resultSet.stopHere()
@@ -43,7 +35,7 @@ object CaosScriptCompletionProvider : CompletionProvider<CompletionParameters>()
         }
 
         val previous = element.previous
-        if (!WHITESPACE.matches(previous?.text?:"")) {
+        if (!WHITESPACE.matches(previous?.text ?: "")) {
             resultSet.stopHere()
             return
         }
@@ -51,8 +43,8 @@ object CaosScriptCompletionProvider : CompletionProvider<CompletionParameters>()
             resultSet.stopHere()
             return
         }
-        var parent:PsiElement? = element
-        while(parent != null && parent !is CaosScriptRvalue && parent !is CaosScriptLvalue && parent !is CaosScriptCommandCall && parent !is CaosScriptIncomplete) {
+        var parent: PsiElement? = element
+        while (parent != null && parent !is CaosScriptRvalue && parent !is CaosScriptLvalue && parent !is CaosScriptCommandCall && parent !is CaosScriptIncomplete) {
             parent = parent.parent
         }
         if (parent == null) {
@@ -62,30 +54,35 @@ object CaosScriptCompletionProvider : CompletionProvider<CompletionParameters>()
         }
         val type = parent.getEnclosingCommandType()
         LOGGER.info("Element: ${element.text} of type ${element.elementType} is of caos value type ${type.value.toLowerCase()}")
-        val singleCommands = CaosDefCommandElementsByNameIndex.Instance.getAll(element.project).filter {
-            if (!(variant.isBlank() || it.isVariant(variant)))
-                return@filter false
-            when(type) {
-                CaosCommandType.COMMAND -> it.isCommand
-                CaosCommandType.RVALUE -> it.isRvalue
-                CaosCommandType.LVALUE -> it.isLvalue
-                CaosCommandType.UNDEFINED -> false
-            }
-        }.map {
-            if (it.commandWords.size > 1) {
-                val commandWords = it.commandWords
-                if (previous?.text?.toLowerCase() == it.commandWords[0].toLowerCase()) {
-                    return@map createCommandTokenLookupElement(element, case, it.commandWords[1], it.isCommand, it.returnTypeString, commandWords[0])
+        val case = element.case
+        val singleCommands = CaosDefCommandElementsByNameIndex.Instance.getAll(element.project)
+                .filter {
+                    if (it.commandName.toUpperCase() !in SKIP_VAR_NAMES)
+                        return@filter false
+                    if (!(variant.isBlank() || it.isVariant(variant)))
+                        return@filter false
+                    when (type) {
+                        CaosCommandType.COMMAND -> it.isCommand
+                        CaosCommandType.RVALUE -> it.isRvalue
+                        CaosCommandType.LVALUE -> it.isLvalue
+                        CaosCommandType.UNDEFINED -> false
+                    }
                 }
-            }
-            createCommandTokenLookupElement(element, case, it.commandName, it.isCommand, it.returnTypeString)
-        }
+                .map {
+                    if (it.commandWords.size > 1) {
+                        val commandWords = it.commandWords
+                        if (previous?.text?.toLowerCase() == it.commandWords[0].toLowerCase()) {
+                            return@map createCommandTokenLookupElement(element, case, it.commandWords[1], it.isCommand, it.returnTypeString, commandWords[0])
+                        }
+                    }
+                    createCommandTokenLookupElement(element, case, it.commandName, it.isCommand, it.returnTypeString)
+                }
         resultSet.addAllElements(singleCommands)
         if (type == CaosCommandType.RVALUE || type == CaosCommandType.LVALUE)
             addVariableCompletions(variant, element, resultSet)
     }
 
-    private fun addVariableCompletions(variant:String, element:PsiElement, resultSet: CompletionResultSet) {
+    private fun addVariableCompletions(variant: String, element: PsiElement, resultSet: CompletionResultSet) {
         when (variant) {
             "C1" -> {
                 (0..2).forEach {
@@ -120,7 +117,7 @@ object CaosScriptCompletionProvider : CompletionProvider<CompletionParameters>()
         }
     }
 
-    private fun createCommandTokenLookupElement(element:PsiElement, case: Case, commandIn: String, isCommand: Boolean, returnType: String, prefixIn: String? = null): LookupElementBuilder {
+    private fun createCommandTokenLookupElement(element: PsiElement, case: Case, commandIn: String, isCommand: Boolean, returnType: String, prefixIn: String? = null): LookupElementBuilder {
         val tailText = if (isCommand)
             "command"
         else
@@ -150,9 +147,9 @@ object CaosScriptCompletionProvider : CompletionProvider<CompletionParameters>()
         return builder
     }
 
-    fun needsSpaceAfter(element:PsiElement, commandString:String) : Boolean {
-        var parent:PsiElement? = element.parent
-        while(parent != null && parent !is CaosScriptRvalue && parent !is CaosScriptLvalue && parent !is CaosScriptCommandCall) {
+    fun needsSpaceAfter(element: PsiElement, commandString: String): Boolean {
+        var parent: PsiElement? = element.parent
+        while (parent != null && parent !is CaosScriptRvalue && parent !is CaosScriptLvalue && parent !is CaosScriptCommandCall) {
             parent = parent.parent
         }
         if (parent == null)
@@ -174,8 +171,3 @@ object CaosScriptCompletionProvider : CompletionProvider<CompletionParameters>()
 }
 
 private val PsiElement.textWithoutCompletionIdString get() = text.substringBefore(CompletionUtilCore.DUMMY_IDENTIFIER)
-
-private enum class Case {
-    UPPER_CASE,
-    LOWER_CASE
-}
