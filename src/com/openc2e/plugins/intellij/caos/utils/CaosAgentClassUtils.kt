5ee8c2b570c01faceebebf285843417b3296ddf6
bedalton <Dsbadal@gmail.com>
2020-05-27 13:22:17 -0700

    Create CaosAgentClassUtils.kt

---
+++ src/com/openc2e/plugins/intellij/caos/utils/CaosAgentClassUtils.kt
@@ -0,0 +1,16 @@
+package com.openc2e.plugins.intellij.caos.utils
+
+data class AgentClass(val family:Int, val genus:Int, val species:Int) {
+    fun like(otherClass:AgentClass) : Boolean {
+        if (family == 0) {
+            if (genus == 0 || otherC)
+        }
+        if (family != otherClass.family && otherClass.family != 0 && family != 0)
+    }
+}
+
+object CaosAgentClassUtils {
+
+    fun toClass
+
+}
