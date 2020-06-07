9f2e976bbcc8a8459e550d1034f60540b7f003a3
bedalton <Dsbadal@gmail.com>
2020-06-06 17:51:34 -0700

    create project type

---
+++ src/com/openc2e/plugins/intellij/caos/project/template/CaosProjectGeneratorInfo.kt
@@ -0,0 +1,5 @@
+package com.openc2e.plugins.intellij.caos.project.template
+
+import com.openc2e.plugins.intellij.caos.lang.CaosVariant
+
+data class CaosProjectGeneratorInfo(val variant:CaosVariant)
---
+++ src/com/openc2e/plugins/intellij/caos/project/template/CaosScriptVariantTemplate.kt
@@ -0,0 +1,44 @@
+package com.openc2e.plugins.intellij.caos.project.template
+
+import com.intellij.ide.util.projectWizard.ModuleBuilder
+import com.intellij.openapi.module.Module
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.ValidationInfo
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.platform.ProjectTemplate
+import com.openc2e.plugins.intellij.caos.project.module.CaosScriptModuleBuilder
+import icons.CaosScriptIcons
+import javax.swing.Icon
+
+
+class CaosScriptVariantTemplate: CaosScriptProjectGenerator(), ProjectTemplate {
+
+
+    override fun createModuleBuilder(): ModuleBuilder {
+        return CaosScriptModuleBuilder()
+    }
+
+    override fun validateSettings(): ValidationInfo? {
+        return null
+    }
+
+    override fun getIcon(): Icon? {
+        return CaosScriptIcons.MODULE_ICON
+    }
+
+    override fun getDescription(): String? {
+        return null
+    }
+
+    override fun generateProject(project: Project, p1: VirtualFile, info: CaosProjectGeneratorInfo, module: Module) {
+        // Ignore
+    }
+
+    override fun getName(): String {
+        return "CAOS Project"
+    }
+
+    override fun getLogo(): Icon? {
+        return icon
+    }
+}
