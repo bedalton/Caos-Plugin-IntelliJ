95f948cec902bc37d36f01cec6990cf01823a043
bedalton <Dsbadal@gmail.com>
2020-06-28 22:11:15 -0700

    update SprDef

--- src/com/openc2e/plugins/intellij/caos/psi/api/CaosScriptCommandLike.kt
+++ src/com/openc2e/plugins/intellij/caos/psi/api/CaosScriptCommandLike.kt
@@ -1,7 +1,5 @@
 package com.openc2e.plugins.intellij.caos.psi.api
 
-import com.openc2e.plugins.intellij.caos.psi.api.CaosScriptCompositeElement
-
 interface CaosScriptCommandLike : CaosScriptCompositeElement {
     val commandString:String
     @JvmDefault
--- src/com/openc2e/plugins/intellij/caos/sprites/spr/def/SprDefEditorImpl.kt
+++ src/com/openc2e/plugins/intellij/caos/sprites/spr/def/SprDefEditorImpl.kt
@@ -1,32 +1,39 @@
 package com.openc2e.plugins.intellij.caos.sprites.spr.def
 
+import com.intellij.openapi.editor.EditorDropHandler
 import com.intellij.openapi.fileEditor.FileEditor
 import com.intellij.openapi.fileEditor.FileEditorLocation
 import com.intellij.openapi.fileEditor.FileEditorState
+import com.intellij.openapi.fileEditor.impl.EditorWindow
+import com.intellij.openapi.module.ModuleUtil
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Key
 import com.intellij.openapi.util.UserDataHolderBase
+import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.openapi.vfs.VirtualFile
+import com.openc2e.plugins.intellij.caos.utils.contents
+import java.awt.datatransfer.DataFlavor
+import java.awt.datatransfer.Transferable
 import java.beans.PropertyChangeListener
-import java.util.*
+import java.io.File
 import javax.swing.JComponent
 
 
-class SprDefEditorImpl : UserDataHolderBase, FileEditor {
-    private var myFile: VirtualFile
-    private var myProject: Project? = null
+class SprDefEditorImpl(private val project: Project, private val myFile: VirtualFile) : UserDataHolderBase(), FileEditor, EditorDropHandler {
 
-    constructor(file: VirtualFile) {
-        myFile = file
-    }
-
-    constructor(project: Project?, file: VirtualFile) {
-        myFile = file
-        myProject = project
-    }
-
+    var getImages: (() -> List<SprDefImageData>) = { emptyList() }
+    var addImages: ((data: List<SprDefImageData>) -> Unit) = { }
+    var originalContents = myFile.contents
     override fun getComponent(): JComponent {
-        return SprDefFileEditor(myFile).`$$$getRootComponent$$$`()
+        return SprDefFileEditor(project, myFile).let {
+            getImages = {
+                it.imageData
+            }
+            addImages = { data ->
+                it.addImages(data)
+            }
+            it.`$$$getRootComponent$$$`()
+        }
     }
 
     override fun getPreferredFocusedComponent(): JComponent? {
@@ -37,9 +44,20 @@ class SprDefEditorImpl : UserDataHolderBase, FileEditor {
         return NAME
     }
 
-    override fun setState(state: FileEditorState) {}
+    override fun setState(state: FileEditorState) {
+    }
+
     override fun isModified(): Boolean {
-        return false
+        val module = ModuleUtil.findModuleForFile(myFile, project)?.moduleFile
+        val contents = getImages().joinToString("\n") { data ->
+            data.virtualFile?.let {
+                if (module != null)
+                    VfsUtil.findRelativePath(module, it, File.separator.first())
+                else
+                    "::" + VfsUtil.findRelativePath(myFile, it, File.separator.first())
+            } ?: data.relativePath
+        }
+        return contents == originalContents
     }
 
     override fun isValid(): Boolean {
@@ -52,14 +70,34 @@ class SprDefEditorImpl : UserDataHolderBase, FileEditor {
         return null
     }
 
-    override fun dispose() {}
-    override fun <T> getUserData(key: Key<T>): T? {
-        return null
+    override fun dispose() {
+        getImages = { emptyList() }
+        addImages = { }
     }
 
-    override fun <T> putUserData(key: Key<T>, value: T?) {}
+    override fun <T> getUserData(key: Key<T>): T? {
+        return myFile.getUserData(key)
+    }
+
+    override fun <T> putUserData(key: Key<T>, value: T?) {
+        myFile.putUserData(key, value)
+    }
 
     companion object {
         private const val NAME = "SPREditor"
+    }
+
+    override fun canHandleDrop(flavor: Array<out DataFlavor>): Boolean {
+        if (flavor.isEmpty())
+            return false
+        return SprImageDataFlavor in flavor || DataFlavor.javaFileListFlavor in flavor
+    }
+
+    override fun handleDrop(transferable: Transferable?, project: Project, editor: EditorWindow) {
+        if (transferable == null)
+            return
+        val data = SprDefEditorUtil.parseTransferable(project, myFile, transferable, getImages().size)
+                ?: return
+        addImages(data)
     }
 }
