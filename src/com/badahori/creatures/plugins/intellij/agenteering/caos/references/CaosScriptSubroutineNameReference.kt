b815be5e2df51cf43446e15b173909a989c9eb92
daniel <dsbadal@gmail.com>
2020-07-07 12:47:32 -0700

    fix subroutine name in header check

--- src/com/badahori/creatures/plugins/intellij/agenteering/caos/references/CaosScriptSubroutineNameReference.kt
+++ src/com/badahori/creatures/plugins/intellij/agenteering/caos/references/CaosScriptSubroutineNameReference.kt
@@ -4,10 +4,7 @@ import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiReferenceBase
 import com.badahori.creatures.plugins.intellij.agenteering.caos.indices.CaosScriptSubroutineIndex
-import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.CaosScriptCGsub
-import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.CaosScriptScriptBodyElement
-import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.CaosScriptSubroutine
-import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.CaosScriptSubroutineName
+import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.*
 import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.util.hasSharedContextOfTypeStrict
 
 class CaosScriptSubroutineNameReference(element: CaosScriptSubroutineName) : PsiReferenceBase<CaosScriptSubroutineName>(element, TextRange.create(0, element.textLength)) {
@@ -22,7 +19,7 @@ class CaosScriptSubroutineNameReference(element: CaosScriptSubroutineName) : Psi
     }
 
     override fun resolve(): PsiElement? {
-        if (myElement.parent !is CaosScriptSubroutine)
+        if (myElement.parent is CaosScriptSubroutineHeader)
             return null
         return CaosScriptSubroutineIndex.instance[myElement.name, myElement.project].firstOrNull {
             myElement.hasSharedContextOfTypeStrict(element, CaosScriptScriptBodyElement::class.java)
