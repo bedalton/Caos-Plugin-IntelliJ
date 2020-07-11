faeb6ef50a5478ca25013291f6fb8b643bc69171
bedalton <Dsbadal@gmail.com>
2020-07-11 16:45:29 -0700

    fix project handling

--- src/com/badahori/creatures/plugins/intellij/agenteering/caos/project/module/CaosScriptModuleBuilder.kt
+++ src/com/badahori/creatures/plugins/intellij/agenteering/caos/project/module/CaosScriptModuleBuilder.kt
@@ -1,16 +1,23 @@
 package com.badahori.creatures.plugins.intellij.agenteering.caos.project.module
 
+import com.badahori.creatures.plugins.intellij.agenteering.caos.project.library.CaosBundleSourcesRegistrationUtil
 import com.intellij.ide.util.projectWizard.ModuleBuilder
 import com.intellij.openapi.module.ModuleType
+import com.intellij.openapi.roots.ModifiableRootModel
 
 class CaosScriptModuleBuilder : ModuleBuilder() {
 
     override fun getModuleType(): ModuleType<*> {
-        return MODULE_TYPE
+        return CaosScriptModuleType.INSTANCE
     }
 
-    companion object {
-        private val MODULE_TYPE = CaosScriptModuleType()
+    override fun setupRootModel(modifiableRootModel: ModifiableRootModel) {
+        super.setupRootModel(modifiableRootModel)
+        CaosBundleSourcesRegistrationUtil.addLibrary(modifiableModel = modifiableRootModel)
+        moduleFileDirectory?.let {
+            modifiableRootModel.addContentEntry("file://$it")
+            true
+        } ?: modifiableRootModel.addContentEntry(moduleFilePath)
     }
 
 }
