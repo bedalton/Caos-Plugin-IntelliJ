9120a8d8933cfbe5ab970d4bb433abf63f59b19a
daniel <dsbadal@gmail.com>
2020-07-17 12:55:36 -0700

    remove unused command

--- src/com/badahori/creatures/plugins/intellij/agenteering/caos/psi/api/CaosScriptIsCommandToken.kt
+++ src/com/badahori/creatures/plugins/intellij/agenteering/caos/psi/api/CaosScriptIsCommandToken.kt
@@ -1,19 +1,15 @@
 package com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api
 
-import com.intellij.psi.PsiNamedElement
 import com.badahori.creatures.plugins.intellij.agenteering.caos.lang.CaosVariant
-import com.badahori.creatures.plugins.intellij.agenteering.caos.lang.orDefault
-import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.impl.containingCaosFile
 import com.badahori.creatures.plugins.intellij.agenteering.caos.references.CaosScriptCommandTokenReference
 import com.badahori.creatures.plugins.intellij.agenteering.caos.utils.equalsIgnoreCase
+import com.intellij.psi.PsiNamedElement
 
 interface CaosScriptIsCommandToken : PsiNamedElement, CaosScriptCompositeElement, CaosScriptShouldBeLowerCase {
     fun isVariant(variants:List<CaosVariant>, strict:Boolean) : Boolean
     val reference:CaosScriptCommandTokenReference
     val commandString:String
 }
-
-val CaosScriptIsCommandToken.variant get() = containingCaosFile?.variant.orDefault()
 
 infix fun CaosScriptIsCommandToken?.equalTo(otherValue:String) : Boolean = this?.text?.equalsIgnoreCase(otherValue) ?: false
 infix fun CaosScriptIsCommandToken?.notEqualTo(otherValue:String) : Boolean = this?.text?.equalsIgnoreCase(otherValue) ?: false
