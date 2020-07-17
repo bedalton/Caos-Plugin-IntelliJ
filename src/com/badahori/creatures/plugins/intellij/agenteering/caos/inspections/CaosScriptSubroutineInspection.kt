8acbc00692af257cc4e77739290ec04e516c577f
daniel <dsbadal@gmail.com>
2020-07-17 12:52:48 -0700

    add manual search for subroutines in event scripts

--- src/com/badahori/creatures/plugins/intellij/agenteering/caos/inspections/CaosScriptSubroutineInspection.kt
+++ src/com/badahori/creatures/plugins/intellij/agenteering/caos/inspections/CaosScriptSubroutineInspection.kt
@@ -3,18 +3,20 @@ package com.badahori.creatures.plugins.intellij.agenteering.caos.inspections
 import com.badahori.creatures.plugins.intellij.agenteering.caos.indices.CaosScriptSubroutineIndex
 import com.badahori.creatures.plugins.intellij.agenteering.caos.lang.CaosBundle
 import com.badahori.creatures.plugins.intellij.agenteering.caos.lang.CaosVariant
-import com.badahori.creatures.plugins.intellij.agenteering.caos.lang.orDefault
 import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.CaosScriptScriptBodyElement
+import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.CaosScriptSubroutine
 import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.CaosScriptSubroutineHeader
 import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.CaosScriptSubroutineName
 import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.impl.containingCaosFile
 import com.badahori.creatures.plugins.intellij.agenteering.caos.utils.hasParentOfType
 import com.intellij.codeInspection.LocalInspectionTool
 import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.openapi.project.DumbAware
 import com.intellij.psi.PsiElementVisitor
 import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.psi.util.PsiTreeUtil
 
-class CaosScriptSubroutineInspection : LocalInspectionTool() {
+class CaosScriptSubroutineInspection : LocalInspectionTool(), DumbAware {
 
     override fun getDisplayName(): String = "Subroutine Is Defined Check"
     override fun getGroupDisplayName(): String = "CaosScript"
@@ -31,7 +33,8 @@ class CaosScriptSubroutineInspection : LocalInspectionTool() {
 
     private fun annotateSubroutineName(element: CaosScriptSubroutineName, holder: ProblemsHolder) {
         val name = element.name
-        val variant = element.containingCaosFile?.variant.orDefault()
+        val variant = element.containingCaosFile?.variant
+                ?: return
         if ((variant == CaosVariant.C1 || variant == CaosVariant.C2) && name.length != 4) {
             holder.registerProblem(element, CaosBundle.message("caos.annotator.command-annotator.subroutine-name-invalid-length", variant))
         }
@@ -41,9 +44,17 @@ class CaosScriptSubroutineInspection : LocalInspectionTool() {
         val matches = CaosScriptSubroutineIndex.instance[name, element.project, searchScope]
         val parent = element.getParentOfType(CaosScriptScriptBodyElement::class.java)
                 ?: return
-        if (matches.any { it.getParentOfType(CaosScriptScriptBodyElement::class.java) == parent }) {
+        if (matches.any { it.getParentOfType(CaosScriptScriptBodyElement::class.java) == parent } || manualSearch(parent, name)) {
             return
         }
+
         holder.registerProblem(element, CaosBundle.message("caos.annotator.command-annotator.subroutine-not-found", name))
+    }
+
+    private fun manualSearch(containingScript:CaosScriptScriptBodyElement, subroutineName:String) : Boolean {
+        return PsiTreeUtil.collectElementsOfType(containingScript, CaosScriptSubroutine::class.java)
+                .any {
+                    it.name == subroutineName
+                }
     }
 }
