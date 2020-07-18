591a87bb470109cb2dbf3dea63cac3045eef14c6
bedalton <3941960+bedalton@users.noreply.github.com>
2020-07-18 04:19:31 -0700

    allow blank lines

--- src/com/badahori/creatures/plugins/intellij/agenteering/caos/formatting/CaosScriptSpacingProcessor.kt
+++ src/com/badahori/creatures/plugins/intellij/agenteering/caos/formatting/CaosScriptSpacingProcessor.kt
@@ -1,15 +1,13 @@
 package com.badahori.creatures.plugins.intellij.agenteering.caos.formatting
 
+import com.badahori.creatures.plugins.intellij.agenteering.caos.lang.CaosScriptFile
 import com.intellij.formatting.Block
 import com.intellij.formatting.Spacing
 import com.intellij.lang.ASTNode
 import com.intellij.psi.codeStyle.CommonCodeStyleSettings
 import com.intellij.psi.formatter.common.AbstractBlock
 import com.badahori.creatures.plugins.intellij.agenteering.caos.lexer.CaosScriptTypes
-import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.CaosScriptEqOp
-import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.CaosScriptExpectsValueOfType
-import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.CaosScriptExpression
-import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.CaosScriptIsCommandToken
+import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.*
 import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.util.isDirectlyPrecededByNewline
 import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.util.next
 import com.badahori.creatures.plugins.intellij.agenteering.caos.utils.orFalse
@@ -21,14 +19,15 @@ internal val LOGGER: Logger by lazy {
 
 class CaosScriptSpacingProcessor(private val myNode: ASTNode, private val mySettings: CommonCodeStyleSettings) {
 
-    private val noneSpace by lazy { Spacing.createSpacing(0, 0, 0, mySettings.KEEP_LINE_BREAKS, 0) }
-    private val oneSpace by lazy { Spacing.createSpacing(1, 1, 0, mySettings.KEEP_LINE_BREAKS, 0) }
-    private val anySpace by lazy { Spacing.createSpacing(0, 1, 0, mySettings.KEEP_LINE_BREAKS, 0) }
+    private val noneSpace by lazy { Spacing.createSpacing(0, 0, 0, mySettings.KEEP_LINE_BREAKS, Int.MAX_VALUE) }
+    private val oneSpace by lazy { Spacing.createSpacing(1, 1, 0, mySettings.KEEP_LINE_BREAKS, Int.MAX_VALUE) }
+    private val anySpace by lazy { Spacing.createSpacing(0, 1, 0, mySettings.KEEP_LINE_BREAKS, Int.MAX_VALUE) }
 
     fun getSpacing(child1: Block?, child2: Block?): Spacing? {
         if (child1 !is AbstractBlock || child2 !is AbstractBlock) {
             return null
         }
+        val keepBlankLines = if((myNode.psi.containingFile as? CaosScriptFile)?.variant?.isNotOld.orFalse()) Int.MAX_VALUE else 0
         val type = myNode.elementType
         val node1 = child1.node
         val type1 = node1.elementType
@@ -39,16 +38,16 @@ class CaosScriptSpacingProcessor(private val myNode: ASTNode, private val mySett
         if (commaTypes.intersect(types).isNotEmpty())
             return noneSpace
         if (myNode.next?.isDirectlyPrecededByNewline().orFalse())
-            Spacing.createSpacing(0, 0, 0, mySettings.KEEP_LINE_BREAKS, 0)
+            Spacing.createSpacing(0, 0, 0, mySettings.KEEP_LINE_BREAKS, keepBlankLines)
         if (node2.isDirectlyPrecededByNewline())
-            return Spacing.createSpacing(0, 0, 0, mySettings.KEEP_LINE_BREAKS, 0)
+            return Spacing.createSpacing(0, 0, 0, mySettings.KEEP_LINE_BREAKS, keepBlankLines)
         if (node1.next?.text.orEmpty().contains("\n")) {
             val lineFeeds = if (mySettings.KEEP_LINE_BREAKS) 1 else 0
-            return Spacing.createSpacing(0, 0, lineFeeds, mySettings.KEEP_LINE_BREAKS, 0)
+            return Spacing.createSpacing(0, 0, lineFeeds, mySettings.KEEP_LINE_BREAKS, keepBlankLines)
         }
         return when (node1.psi) {
-            is CaosScriptExpectsValueOfType -> spaceAfterIsValueOfType(node2)
-            is CaosScriptIsCommandToken -> spaceAfterIsCommandToken(node2)
+            is CaosScriptExpectsValueOfType -> spaceAfterIsValueOfType(node2,keepBlankLines)
+            is CaosScriptIsCommandToken -> spaceAfterIsCommandToken(node2,keepBlankLines)
             is CaosScriptExpression -> oneSpace
             is CaosScriptEqOp -> oneSpace
             else -> anySpace
@@ -56,22 +55,22 @@ class CaosScriptSpacingProcessor(private val myNode: ASTNode, private val mySett
 
     }
 
-    private fun spaceAfterIsCommandToken(node2: ASTNode): Spacing? {
+    private fun spaceAfterIsCommandToken(node2: ASTNode, keepBlankLines:Int): Spacing? {
         if (node2.elementType in commaTypes) {
-            return Spacing.createSpacing(0, 0, 0, false, 0)
+            return Spacing.createSpacing(0, 0, 0, false, keepBlankLines)
         }
         if (node2.psi is CaosScriptExpectsValueOfType) {
-            return Spacing.createSpacing(1, 1, 0, false, 0)
+            return Spacing.createSpacing(1, 1, 0, false, keepBlankLines)
         }
         return null
     }
 
-    private fun spaceAfterIsValueOfType(node2: ASTNode): Spacing? {
+    private fun spaceAfterIsValueOfType(node2: ASTNode, keepBlankLines:Int): Spacing? {
         if (node2.elementType in commaTypes || node2.isDirectlyPrecededByNewline()) {
-            return Spacing.createSpacing(0, 0, 0, false, 0)
+            return Spacing.createSpacing(0, 0, 0, false, keepBlankLines)
         }
         if (node2.psi is CaosScriptExpectsValueOfType) {
-            return Spacing.createSpacing(1, 1, 0, false, 0)
+            return Spacing.createSpacing(1, 1, 0, false, keepBlankLines)
         }
         return null
     }
