9698991e8ca5ca3f3827aacde9226b3ba5a0e611
daniel <dsbadal@gmail.com>
2020-07-27 10:50:34 -0700

    fix type handler delegate

--- src/com/badahori/creatures/plugins/intellij/agenteering/caos/completion/CaosScriptTypedHandler.kt
+++ src/com/badahori/creatures/plugins/intellij/agenteering/caos/completion/CaosScriptTypedHandler.kt
@@ -3,20 +3,24 @@ package com.badahori.creatures.plugins.intellij.agenteering.caos.completion
 import com.badahori.creatures.plugins.intellij.agenteering.caos.lang.CaosScriptFile
 import com.badahori.creatures.plugins.intellij.agenteering.caos.lexer.CaosScriptTypes
 import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.*
+import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.util.LOGGER
 import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.util.getSelfOrParentOfType
 import com.badahori.creatures.plugins.intellij.agenteering.caos.utils.EditorUtil
 import com.badahori.creatures.plugins.intellij.agenteering.caos.utils.element
 import com.intellij.codeInsight.editorActions.TypedHandlerDelegate
+import com.intellij.codeInsight.template.impl.editorActions.TypedActionHandlerBase
+import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.application.runUndoTransparentWriteAction
 import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.actionSystem.TypedActionHandler
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiFile
 import com.intellij.psi.codeStyle.CodeStyleManager
 import com.intellij.psi.tree.TokenSet
 
-class CaosScriptTypedHandler : TypedHandlerDelegate() {
+class CaosScriptTypedHandler(private val originalHandler:TypedActionHandler?) : TypedActionHandlerBase(originalHandler) {
 
-    private val handle = listOf('\'', '"', "[")
+    private val handle = listOf('\'', '"')
     private val quoteTokenSet = TokenSet.create(
             CaosScriptTypes.CaosScript_DOUBLE_QUOTE,
             CaosScriptTypes.CaosScript_SINGLE_QUOTE,
@@ -33,33 +37,29 @@ class CaosScriptTypedHandler : TypedHandlerDelegate() {
             'e', 'E' // rep[e]
     )
 
-    override fun charTyped(c: Char, project: Project, editor: Editor, file: PsiFile): Result {
-        if (file !is CaosScriptFile)
-            return Result.CONTINUE
-        if (handleQuote(c, editor)) {
-            return Result.CONTINUE
+
+    override fun execute(editor: Editor, char: Char, context: DataContext) {
+        originalHandler?.execute(editor, char, context)
+        if (handleQuote(char, editor)) {
+            return
         }
-        if (dedentIfBlockEnd(c, project, editor)) {
-            return Result.CONTINUE
-        }
-        return Result.CONTINUE
+        //if (dedentIfBlockEnd(c, project, editor)) {
+        //  return Result.CONTINUE
+        // }
     }
 
     private fun handleQuote(c: Char, editor: Editor): Boolean {
-        if (c !in handle)
+        if (c != '"' && c != '\'')
             return false
         val element = editor.element
                 ?: return false
-        val quotes = element.node?.getChildren(quoteTokenSet)
-                ?: return false
-        if (quotes.isEmpty())
-            return false
-        if (quotes.size > 1)
-            return false
+        element.getSelfOrParentOfType(CaosScriptStringLike::class.java)?.let {
+            if (it.isClosed)
+                return true
+        }
         val replacement = when (c) {
             '\'' -> "'"
             '"' -> "\""
-            '[' -> "]"
             else -> return false
         }
         EditorUtil.insertText(editor, replacement, false)
