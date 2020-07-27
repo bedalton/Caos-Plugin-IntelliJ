5c071c1f0925410dd7a3209b0134a1a7b8891aab
daniel <dsbadal@gmail.com>
2020-07-27 10:35:50 -0700

    add isClosed to string like elements

--- src/com/badahori/creatures/plugins/intellij/agenteering/caos/psi/api/CaosScriptStringLike.kt
+++ src/com/badahori/creatures/plugins/intellij/agenteering/caos/psi/api/CaosScriptStringLike.kt
@@ -2,4 +2,5 @@ package com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api
 
 interface CaosScriptStringLike : CaosScriptCompositeElement{
     val stringValue:String
+    val isClosed:Boolean
 }
