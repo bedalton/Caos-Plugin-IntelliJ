42cfd73878af67474ec223c19739650a3957f998
daniel <dsbadal@gmail.com>
2020-07-31 08:20:32 -0700

    cleanup imports

--- src/com/badahori/creatures/plugins/intellij/agenteering/caos/completion/BitFlagsCompletions.kt
+++ src/com/badahori/creatures/plugins/intellij/agenteering/caos/completion/BitFlagsCompletions.kt
@@ -11,7 +11,6 @@ import com.intellij.codeInsight.completion.InsertHandler
 import com.intellij.codeInsight.completion.InsertionContext
 import com.intellij.codeInsight.intention.IntentionAction
 import com.intellij.codeInsight.lookup.LookupElement
-import com.intellij.openapi.application.ModalityState
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogWrapper
