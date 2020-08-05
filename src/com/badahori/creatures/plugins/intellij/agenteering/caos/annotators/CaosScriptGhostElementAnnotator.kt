1b9b53742726c31f2eedd1db24b2d655fec0787b
bedalton <Dsbadal@gmail.com>
2020-08-05 16:18:30 -0700

    add comments

--- src/com/badahori/creatures/plugins/intellij/agenteering/caos/annotators/CaosScriptGhostElementAnnotator.kt
+++ src/com/badahori/creatures/plugins/intellij/agenteering/caos/annotators/CaosScriptGhostElementAnnotator.kt
@@ -12,12 +12,21 @@ import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.util.next
 import com.badahori.creatures.plugins.intellij.agenteering.caos.utils.matchCase
 import com.intellij.openapi.progress.ProgressIndicatorProvider
 
+/**
+ * Marks 'Ghost' elements which are required elements in CAOS, that are not required in BNF grammar
+ * Grammar allows incomplete control statements to keep from breaking AST tree
+ * In grammar, ENUM..NEXT can be parsed without NEXT and DOIF..ENDI without ENDI
+ */
 class CaosScriptGhostElementAnnotator : Annotator {
+    /**
+     * Look through all elements in file and annotate as necessary
+     */
     override fun annotate(element: PsiElement, annotationHolder: AnnotationHolder) {
         ProgressIndicatorProvider.checkCanceled()
         val annotationWrapper = AnnotationHolderWrapper(annotationHolder)
         if (element !is CaosScriptCompositeElement)
             return
+        // Get the expected terminator
         val terminator = when (element) {
             is CaosScriptDoifStatement -> "ENDI"
             is CaosScriptSubroutine -> "RETN"
@@ -28,6 +37,7 @@ class CaosScriptGhostElementAnnotator : Annotator {
                 null
             }
             is CaosScriptEnumSceneryStatement -> "NSCN"
+            // Ghost element here is SETV which is optional in grammar for C1 and C2, but not optional in CAOS
             is CaosScriptCAssignment -> {
                 if (element.firstChild is CaosScriptLvalue) {
                     annotationWrapper.newErrorAnnotation("${element.text.substring(0,4).toUpperCase()} is an LValue and must be used with an lvalue operator such as SETV ")
@@ -37,26 +47,41 @@ class CaosScriptGhostElementAnnotator : Annotator {
                 }
                 null
             }
+            // Element does not have possible ghost element
             else -> null
         } ?: return
+        // Annotate the statement
         annotate(element, terminator, annotationWrapper)
     }
 
+    /**
+     * Actually annotate element when there is only one ghost element option
+     */
     private fun <PsiT:PsiElement> annotate(element:PsiT, expectedToken:String, annotationWrapper: AnnotationHolderWrapper) {
-        if (element.lastChild.text.toUpperCase().endsWith(expectedToken))
+        // If block ends with expected command, not need to do more
+        if (element.lastChild.text.trim().toUpperCase().endsWith(expectedToken))
             return
+        // Get next element after control block
         var range:PsiElement? = element.next
+        // Ensure that range is not whitespace
         while (range != null && range.text.isBlank()) {
             range = range.next
         }
+        // There is no non-whitespace next element, so use last child of control statement
         if (range == null)
             range = element.lastChild
+
+        // Add annotation
         annotationWrapper.newErrorAnnotation("Unterminated ${element.text.substring(0,4).toUpperCase()} statement. Expected ${expectedToken.toUpperCase()}")
                 .range(range!!)
                 .withFix(AppendStatementTerminator(element, expectedToken))
                 .create()
     }
 
+    /**
+     * Annotate multiple options for ghost element.
+     * Only used for LOOP.. UNTL/EVER
+     */
     private fun <PsiT:PsiElement> annotate(element:PsiT, expectedTokens:List<String>, annotationWrapper: AnnotationHolderWrapper) {
         for(expectedToken in expectedTokens) {
             ProgressIndicatorProvider.checkCanceled()
