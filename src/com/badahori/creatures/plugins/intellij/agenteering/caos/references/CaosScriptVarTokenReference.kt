6ff5a0840f507d4f4bc9231e9c9d8bc48420d609
bedalton <3941960+bedalton@users.noreply.github.com>
2020-08-07 09:46:58 -0700

    add soft parameter to constructor

--- src/com/badahori/creatures/plugins/intellij/agenteering/caos/references/CaosScriptVarTokenReference.kt
+++ src/com/badahori/creatures/plugins/intellij/agenteering/caos/references/CaosScriptVarTokenReference.kt
@@ -1,33 +1,27 @@
 package com.badahori.creatures.plugins.intellij.agenteering.caos.references
 
 import com.badahori.creatures.plugins.intellij.agenteering.caos.def.indices.CaosDefCommandElementsByNameIndex
-import com.badahori.creatures.plugins.intellij.agenteering.caos.def.psi.api.CaosDefCommand
-import com.badahori.creatures.plugins.intellij.agenteering.caos.def.psi.api.CaosDefCommandDefElement
+import com.badahori.creatures.plugins.intellij.agenteering.caos.def.psi.api.CaosDefCommandWord
 import com.badahori.creatures.plugins.intellij.agenteering.caos.lang.CaosVariant
-import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.CaosScriptNamedVar
 import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.CaosScriptVarToken
 import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.impl.containingCaosFile
 import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.util.LOGGER
-import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiElementResolveResult
 import com.intellij.psi.PsiPolyVariantReferenceBase
 import com.intellij.psi.ResolveResult
 
-class CaosScriptVarTokenReference(element: CaosScriptVarToken) : PsiPolyVariantReferenceBase<CaosScriptVarToken>(element, TextRange.create(0, element.name.length)) {
+class CaosScriptVarTokenReference(element: CaosScriptVarToken) : PsiPolyVariantReferenceBase<CaosScriptVarToken>(element, TextRange.create(0, element.name.length), true) {
 
     private val name:String by lazy {
         element.text.toUpperCase()
     }
 
     override fun isReferenceTo(element: PsiElement): Boolean {
-        if (element is CaosDefCommand) {
+        if (element is CaosDefCommandWord) {
+            LOGGER.info("Check if reference to DEF element: ${element.text.toUpperCase()}")
             return element in multiResolve(false).map { it.element }
-        }
-        if (element !is CaosScriptVarToken) {
-            LOGGER.info("Element: ${element.text} is not var-token")
-            return false
         }
         LOGGER.info("IsVarToken: $name");
         if (element.text.toUpperCase() != name)
