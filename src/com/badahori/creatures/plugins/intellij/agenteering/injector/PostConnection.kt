40c75116c0ea0ec776e883c0ef8be0fb33e825d6
daniel <dsbadal@gmail.com>
2020-08-12 11:00:19 -0700

    add caos server injector

--- src/com/badahori/creatures/plugins/intellij/agenteering/injector/PostConnection.kt
+++ src/com/badahori/creatures/plugins/intellij/agenteering/injector/PostConnection.kt
@@ -1,4 +1,80 @@
 package com.badahori.creatures.plugins.intellij.agenteering.injector
 
-class PostConnection {
+import com.badahori.creatures.plugins.intellij.agenteering.caos.lang.CaosVariant
+import com.intellij.openapi.project.Project
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+
+
+internal class PostConnection(urlString:String, variant:CaosVariant) : CaosConnection {
+
+    private val url:URL by lazy {
+        val pathComponent = variant.code.toLowerCase()
+        val path =  if (urlString.endsWith("/")) {
+            urlString + pathComponent
+        } else {
+            "$urlString/$pathComponent"
+        }
+        if (path.startsWith("http"))
+            URL(path)
+        else
+            URL("http://$path")
+    }
+
+    override fun inject(caos: String): InjectionStatus {
+        val connection: HttpURLConnection = try {
+            url.openConnection() as HttpURLConnection
+        } catch (e:IOException) {
+            return InjectionStatus.BadConnection("Failed to open caos connection. Error: ${e.message}")
+        }
+            connection.setRequestMethod("POST")
+        try {
+            connection.outputStream.let {
+                it.write(caos.toByteArray())
+                it.flush()
+                it.close()
+            }
+        } catch (e:Exception) {
+            return InjectionStatus.BadConnection("Failed to write caos code to stream. Error: ${e.message}")
+        }
+        val response = try {
+            val inputStream = BufferedReader(
+                    InputStreamReader(connection.getInputStream()))
+            var inputLine: String?
+            val content = StringBuffer()
+            while (inputStream.readLine().also { inputLine = it } != null) {
+                content.append(inputLine)
+            }
+            inputStream.close()
+            content.toString()
+        } catch (e:Exception) {
+            return InjectionStatus.Bad("Failed to read response from caos server. Error: ${e.message}")
+        }
+        val json = com.google.gson.JsonParser().parse(response)
+        return json.asJsonObject.let {
+            val message = try {
+                it.get("response").asString
+            } catch (e:Exception) {
+                return InjectionStatus.Bad("Failed to parse server response")
+            }
+            when (it.get("status").asString) {
+                "!ERR" -> InjectionStatus.Bad(message)
+                "!CON" -> InjectionStatus.BadConnection(message)
+                else -> InjectionStatus.Ok(message)
+            }
+        }
+    }
+
+    override fun disconnect(): Boolean = true
+
+    override fun isConnected(): Boolean = true
+
+    override fun connect(silent: Boolean): Boolean = true
+
+    override fun showAttribution(project: Project, variant: CaosVariant) {
+        TODO("Not yet implemented")
+    }
 }
