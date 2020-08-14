3f3f71c979f8fa2a78bf9bca6c1a03540b592c09
daniel <dsbadal@gmail.com>
2020-08-14 14:57:14 -0700

    add CV+ stim/sway/chem folding

--- src/com/badahori/creatures/plugins/intellij/agenteering/caos/hints/CaosScriptStimFoldingBuilder.kt
+++ src/com/badahori/creatures/plugins/intellij/agenteering/caos/hints/CaosScriptStimFoldingBuilder.kt
@@ -1,13 +1,17 @@
 package com.badahori.creatures.plugins.intellij.agenteering.caos.hints
 
+import com.badahori.creatures.plugins.intellij.agenteering.caos.deducer.CaosNumber
 import com.badahori.creatures.plugins.intellij.agenteering.caos.deducer.CaosVar
+import com.badahori.creatures.plugins.intellij.agenteering.caos.deducer.isNumeric
 import com.badahori.creatures.plugins.intellij.agenteering.caos.def.psi.api.CaosDefCommandDefElement
 import com.badahori.creatures.plugins.intellij.agenteering.caos.lang.CaosVariant
 import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.CaosScriptCommandCall
 import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.CaosScriptExpectsAgent
 import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.CaosScriptExpectsValueOfType
 import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.impl.containingCaosFile
+import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.util.endOffset
 import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.util.getParentOfType
+import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.util.startOffset
 import com.badahori.creatures.plugins.intellij.agenteering.caos.utils.nullIfEmpty
 import com.intellij.lang.ASTNode
 import com.intellij.lang.folding.FoldingBuilderEx
@@ -19,8 +23,6 @@ import com.intellij.openapi.project.DumbService
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 import com.intellij.psi.util.PsiTreeUtil
-import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.util.endOffset
-import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.util.startOffset
 import kotlin.math.abs
 
 class CaosScriptStimFoldingBuilder : FoldingBuilderEx() {
@@ -53,24 +55,31 @@ class CaosScriptStimFoldingBuilder : FoldingBuilderEx() {
         }
     }
 
-    private fun foldC1ChemCall(commandCall: CaosScriptCommandCall, commandDef: CaosDefCommandDefElement): String? {
+    private fun foldC1ChemCall(commandCall: CaosScriptCommandCall, caosDef: CaosDefCommandDefElement) = foldChemCallGeneric(commandCall, caosDef)
+
+    private fun foldC3ChemCall(commandCall: CaosScriptCommandCall, caosDef: CaosDefCommandDefElement) = foldChemCallGeneric(commandCall, caosDef)
+
+    private fun foldChemCallGeneric(commandCall: CaosScriptCommandCall, commandDef: CaosDefCommandDefElement): String? {
         val arguments = commandCall.arguments
         val parameters = commandDef.parameterStructs
         val firstIndex = parameters
                 .sortedBy { it.parameterNumber }
                 .firstOrNull { parameter ->
-                    parameter.name.toLowerCase().startsWith("chem")
+                    firstFoldParameterRegex.matches(parameter.name)
                 }
                 ?.parameterNumber
                 ?: return null
         if (firstIndex >= arguments.size)
             return null
         val chemParameters = arguments.subList(firstIndex, arguments.size)
+        val variant = commandCall.containingCaosFile?.variant
+                ?: return null
         if (chemParameters.size % 2 != 0) {
             return null
         }
         val numParameters = chemParameters.size / 2
         val stringBuilder = StringBuilder()
+        val isStim = numParameters == 1 && STIM.matches(commandCall.commandString)
         for (i in 0 until numParameters) {
             val pos = i * 2
             val argument = chemParameters[pos]
@@ -85,28 +94,33 @@ class CaosScriptStimFoldingBuilder : FoldingBuilderEx() {
                     ?: argument.text
                     ?: continue
             val amount = chemParameters[pos + 1].toCaosVar()
-            if (amount is CaosVar.CaosLiteral.CaosInt) {
-                if (amount.value == 0L)
-                    continue
-                stringBuilder.append(", +")
-                stringBuilder.append(amount.value).append(" ")
-            } else if (amount is CaosVar.CaosLiteral.CaosFloat) {
-                if (abs(amount.value) < 0.0001)
-                    continue
-                stringBuilder.append(", +")
-                stringBuilder.append(amount.value).append(" ")
-            } else {
-                stringBuilder.append(", +")
-            }
+            formatChemAmount(variant, stringBuilder, value, amount, isStim)
             stringBuilder.append(value)
         }
         return stringBuilder.toString().trim(' ', ',').nullIfEmpty()
     }
 
-    private fun foldC3ChemCall(commandCall: CaosScriptCommandCall, commandDef: CaosDefCommandDefElement): String? {
-        // todo implement CV+ folding for STIM calls
-        return null
+    private fun formatChemAmount(variant: CaosVariant, stringBuilder: StringBuilder, value:String, amountVar:CaosVar, isStim:Boolean = false) {
+
+        val amount = if (amountVar is CaosVar.CaosLiteral.CaosInt)
+            amountVar.value * 1.0f
+        else if (amountVar is CaosVar.CaosLiteral.CaosFloat)
+            amountVar.value
+        else
+            0.0f
+
+        if (isStim) {
+            stringBuilder.append(value).append(" * ").append(amount)
+            return
+        }
+        if (abs(amount) < 0.0003)
+            return
+        stringBuilder.append(", ")
+        if (amount > 0)
+            stringBuilder.append("+")
+        stringBuilder.append(amount).append(" ").append(value)
     }
+
 
     override fun buildFoldRegions(root: PsiElement, document: Document, quick: Boolean): Array<FoldingDescriptor> {
         val group = FoldingGroup.newGroup("CaosScript_STIM_FOLDING")
@@ -126,27 +140,18 @@ class CaosScriptStimFoldingBuilder : FoldingBuilderEx() {
     private fun getCommandCallFoldingRegion(commandCall: CaosScriptCommandCall, group: FoldingGroup): FoldingDescriptor? {
         if (!shouldFold(commandCall))
             return null
-        val variant = commandCall.containingCaosFile?.variant
-                ?: return null
-        return when (variant) {
-            CaosVariant.C1, CaosVariant.C2 -> getC1StimFoldingDescriptor(commandCall, group)
-            else -> {
-                // TODO implement CV+ folding
-                null
-            }
-
-        }
+        return getFoldingDescriptor(commandCall, group)
     }
 
-    private fun getC1StimFoldingDescriptor(commandCall: CaosScriptCommandCall, group: FoldingGroup): FoldingDescriptor? {
+    private fun getFoldingDescriptor(commandCall: CaosScriptCommandCall, group: FoldingGroup): FoldingDescriptor? {
+        if (shouldFoldAll.matches(commandCall.commandString))
+            return FoldingDescriptor(commandCall.node, commandCall.textRange, group)
         val arguments = commandCall.arguments
-
-
-        val start = (if (arguments.firstOrNull() is CaosScriptExpectsAgent)
+        val firstArgument = if (arguments.firstOrNull() is CaosScriptExpectsAgent)
             arguments.getOrNull(1)
         else
             arguments.firstOrNull()
-                )
+        val start = firstArgument
                 ?.startOffset
                 ?: return null
         val end = arguments.lastOrNull()?.endOffset
@@ -159,7 +164,14 @@ class CaosScriptStimFoldingBuilder : FoldingBuilderEx() {
     }
 
     private fun shouldFold(commandCall: CaosScriptCommandCall): Boolean {
-        return commandCall.commandString.toUpperCase().startsWith("STIM")
+        return shouldFold.matches(commandCall.commandString) && commandCall.arguments.filter { it.toCaosVar().isNumeric }.size > 1
+    }
+
+    companion object {
+        private val STIM = "[Ss][Tt][Ii][Mm]([ ][^ ]{4})*".toRegex()
+        private val shouldFoldAll = "([Dd][Rr][Ii][Vv]|[Cc][Hh][Ee][Mm])".toRegex()
+        private val shouldFold = "([Ss][Tt][Ii][Mm]|[Dd][Rr][Ii][Vv]|[Ss][Ww][Aa][Yy]|[Cc][Hh][Ee][Mm])([ ][^ ]{4})*".toRegex()
+        private val firstFoldParameterRegex = "([Dd][Rr][Ii][Vv]([Ee])?)|([Cc][Hh][Ee][Mm]([Ii][Cc][Aa][Ll][Ss]?)?)|([Ss][Tt][Ii][Mm]([Uu][Ll][Uu][Ss])?)[0-9]*".toRegex()
     }
 
 }
