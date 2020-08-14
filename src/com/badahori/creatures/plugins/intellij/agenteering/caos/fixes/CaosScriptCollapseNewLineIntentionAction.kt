c9c7c72146bb0db73bd27b43698472ff65079577
bedalton <3941960+bedalton@users.noreply.github.com>
2020-08-14 15:19:08 -0700

    Fix: comments and newline not collapsing to single line

--- src/com/badahori/creatures/plugins/intellij/agenteering/caos/fixes/CaosScriptCollapseNewLineIntentionAction.kt
+++ src/com/badahori/creatures/plugins/intellij/agenteering/caos/fixes/CaosScriptCollapseNewLineIntentionAction.kt
@@ -6,6 +6,7 @@ import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.CaosScri
 import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.util.CaosScriptPsiElementFactory
 import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.util.lineNumber
 import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.util.next
+import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.util.previous
 import com.badahori.creatures.plugins.intellij.agenteering.caos.utils.document
 import com.intellij.codeInsight.intention.IntentionAction
 import com.intellij.codeInspection.LocalQuickFix
@@ -13,9 +14,7 @@ import com.intellij.codeInspection.ProblemDescriptor
 import com.intellij.openapi.application.runWriteAction
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
-import com.intellij.psi.PsiDocumentManager
-import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiFile
+import com.intellij.psi.*
 import com.intellij.psi.codeStyle.CodeStyleManager
 import com.intellij.psi.util.PsiTreeUtil
 
@@ -23,7 +22,6 @@ class CaosScriptCollapseNewLineIntentionAction(private val collapseChar: Collaps
     override fun startInWriteAction(): Boolean = true
 
     override fun getFamilyName(): String = CaosBundle.message("caos.intentions.family")
-
 
     override fun isAvailable(project: Project, editor: Editor?, file: PsiFile?): Boolean {
         if (file == null)
@@ -45,84 +43,94 @@ class CaosScriptCollapseNewLineIntentionAction(private val collapseChar: Collaps
         invoke(project, file)
     }
 
-    private fun invoke(project: Project, file:PsiFile) {
+    private fun invoke(project: Project, file: PsiFile) {
         val document = PsiDocumentManager.getInstance(project).getCachedDocument(file) ?: file.document
         if (document != null) {
             PsiDocumentManager.getInstance(project).commitDocument(document)
         }
-        val comments = PsiTreeUtil.collectElementsOfType(file, CaosScriptComment::class.java)
-        for(comment in comments) {
-            comment.delete()
-        }
-        val newLines = PsiTreeUtil.collectElementsOfType(file, CaosScriptSpaceLikeOrNewline::class.java)
-        for (newLine in newLines) {
-            if (newLine.isValid)
-                replaceWithSpaceOrComma(newLine, collapseChar)
-        }
-        while(trailingText.matches(file.firstChild.text))
-            file.firstChild.delete()
-        while(trailingText.matches(file.lastChild.text))
-            file.lastChild.delete()
-        runWriteAction {
-            file.document?.let {
-                PsiDocumentManager.getInstance(project).doPostponedOperationsAndUnblockDocument(it)
-                CodeStyleManager.getInstance(project).reformat(file, true)
-            }
-        }
+        collapseLines(file, collapseChar)
     }
 
 
     companion object {
 
+        private val whitespaceOrComma = "(\\s|,)+".toRegex()
         private val trailingText = "[ \n,\t]+".toRegex()
 
-        fun collapseLinesInCopy(fileIn: PsiFile, collapseChar:CollapseChar = CollapseChar.COMMA) : PsiFile {
+        fun collapseLinesInCopy(fileIn: PsiFile, collapseChar: CollapseChar = CollapseChar.COMMA): PsiFile {
             val project = fileIn.project
-            var document = PsiDocumentManager.getInstance(project).getCachedDocument(fileIn) ?: fileIn.document
+            val document = PsiDocumentManager.getInstance(project).getCachedDocument(fileIn) ?: fileIn.document
             if (document != null) {
                 PsiDocumentManager.getInstance(project).commitDocument(document)
             }
             val file = fileIn.copy().let { it as? PsiFile ?: it.containingFile }
+            return collapseLines(file, collapseChar)
+        }
+
+        fun collapseLines(file: PsiFile, collapseChar: CollapseChar): PsiFile {
+            val project = file.project
             val comments = PsiTreeUtil.collectElementsOfType(file, CaosScriptComment::class.java)
-            for(comment in comments) {
-                comment.delete()
+            for (comment in comments) {
+                if (comment.isValid) {
+                    var replacement = CaosScriptPsiElementFactory.spaceLikeOrNewlineSpace(project)
+                    replacement = comment.replace(replacement)
+                    replacement.delete()
+                }
+            }
+            file.document?.let {
+                PsiDocumentManager.getInstance(project).doPostponedOperationsAndUnblockDocument(it)
+                CodeStyleManager.getInstance(project).reformat(file, true)
             }
             val newLines = PsiTreeUtil.collectElementsOfType(file, CaosScriptSpaceLikeOrNewline::class.java)
             for (newLine in newLines) {
                 if (newLine.isValid)
                     replaceWithSpaceOrComma(newLine, collapseChar)
             }
-            while(trailingText.matches(file.firstChild.text))
+            while (trailingText.matches(file.firstChild.text))
                 file.firstChild.delete()
-            while(trailingText.matches(file.lastChild.text))
+            while (trailingText.matches(file.lastChild.text))
                 file.lastChild.delete()
-            document = PsiDocumentManager.getInstance(project).getCachedDocument(file) ?: file.document
-            if (document != null) {
-                PsiDocumentManager.getInstance(project).doPostponedOperationsAndUnblockDocument(document)
-                PsiDocumentManager.getInstance(project).commitDocument(document)
+            runWriteAction {
+                file.document?.let {
+                    PsiDocumentManager.getInstance(project).doPostponedOperationsAndUnblockDocument(it)
+                    CodeStyleManager.getInstance(project).reformat(file, true)
+                }
             }
             return file
         }
 
-        private fun replaceWithSpaceOrComma(next: PsiElement?, collapseChar: CollapseChar) {
-            if (next == null)
+        private fun replaceWithSpaceOrComma(nextIn: PsiElement?, collapseChar: CollapseChar) {
+
+            if (nextIn == null)
                 return
-            if ((next.text != "\n" && !next.text.contains('\n')) || next.text == collapseChar.char)
-                return
-            val element = if (collapseChar == CollapseChar.SPACE)
-                CaosScriptPsiElementFactory.spaceLikeOrNewlineSpace(next.project)
-            else
-                CaosScriptPsiElementFactory.comma(next.project)
-            next.replace(element)
-            var superNext = next.next
+
+            val pointer = SmartPointerManager.createPointer(nextIn)
+            var previous = nextIn.previous
+            while(previous is PsiWhiteSpace) {
+                previous.delete()
+                previous = previous.previous
+            }
+            val next = pointer.element
                     ?: return
-            while(superNext.text.contains("\n") || superNext.text.contains(collapseChar.char)) {
+            val previousIsNotCollapseChar = next.previous?.text != collapseChar.char
+            if ((next.text != "\n" && !next.text.contains('\n')) || (next.text == collapseChar.char && previousIsNotCollapseChar))
+                return
+            var superNext = if (previousIsNotCollapseChar) {
+                val element = if (collapseChar == CollapseChar.SPACE)
+                    CaosScriptPsiElementFactory.spaceLikeOrNewlineSpace(next.project)
+                else
+                    CaosScriptPsiElementFactory.comma(next.project)
+                val replaced = next.replace(element)
+                replaced.next
+            } else {
+                next
+            } ?: return
+            while (whitespaceOrComma.matches(superNext.text)) {
                 superNext.delete()
                 superNext = superNext.next ?: return
             }
         }
     }
-
 }
 
 enum class CollapseChar(internal val text: String, internal val char: String) {
