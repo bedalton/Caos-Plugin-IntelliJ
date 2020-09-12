e1a865d64a49b44803c95e8afc97799cd997d319
daniel <dsbadal@gmail.com>
2020-09-12 03:55:26 -0700

    remove unneccessary curly braces

--- src/com/badahori/creatures/plugins/intellij/agenteering/bundles/cobs/lang/CobLanguage.kt
+++ src/com/badahori/creatures/plugins/intellij/agenteering/bundles/cobs/lang/CobLanguage.kt
@@ -1,8 +1,7 @@
 package com.badahori.creatures.plugins.intellij.agenteering.bundles.cobs.lang
 
 import com.intellij.lang.Language
+import com.intellij.openapi.fileTypes.LanguageFileType
 
 
-object CobLanguage : Language("CreaturesCOB") {
-
-}
+object CobLanguage : Language("CreaturesCOB")
