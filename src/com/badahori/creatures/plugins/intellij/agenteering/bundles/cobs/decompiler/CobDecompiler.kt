0f93a589f2f01c65805600fd4c0512b1207c7cac
bedalton <3941960+bedalton@users.noreply.github.com>
2020-09-12 03:22:55 -0700

    Create sealed COB file data blocks for C1/C2

--- src/com/badahori/creatures/plugins/intellij/agenteering/bundles/cobs/decompiler/CobDecompiler.kt
+++ src/com/badahori/creatures/plugins/intellij/agenteering/bundles/cobs/decompiler/CobDecompiler.kt
@@ -1,31 +1,22 @@
 package com.badahori.creatures.plugins.intellij.agenteering.bundles.cobs.decompiler
 
-import com.badahori.creatures.plugins.intellij.agenteering.bundles.cobs.decompiler.CobBlock.*
-import com.badahori.creatures.plugins.intellij.agenteering.bundles.general.AgentScript
-import com.badahori.creatures.plugins.intellij.agenteering.bundles.general.AgentScriptType
-import com.badahori.creatures.plugins.intellij.agenteering.sprites.flip
-import com.badahori.creatures.plugins.intellij.agenteering.sprites.s16.S16SpriteFile
-import com.badahori.creatures.plugins.intellij.agenteering.sprites.s16.S16SpriteFrame
-import com.badahori.creatures.plugins.intellij.agenteering.sprites.spr.SprSpriteFrame
-import com.badahori.creatures.plugins.intellij.agenteering.sprites.sprite.ColorEncoding
 import com.badahori.creatures.plugins.intellij.agenteering.utils.*
 import com.intellij.util.io.toByteArray
 import java.io.ByteArrayOutputStream
 import java.nio.ByteBuffer
-import java.util.*
 import java.util.zip.InflaterOutputStream
 
 object CobDecompiler {
 
-    public fun decompile(buffer: ByteBuffer): List<CobBlock> {
+    public fun decompile(buffer: ByteBuffer): CobFileData {
         val header = buffer.cString(4)
         buffer.position(0)
         return if (header == "cob2") {
             decompileC2Cob(buffer)
         } else try {
             val decompressed = ByteArrayOutputStream()
-            val inflator = InflaterOutputStream(decompressed)
-            inflator.write(buffer.toByteArray())
+            val decompressor = InflaterOutputStream(decompressed)
+            decompressor.write(buffer.toByteArray())
             decompileC2Cob(ByteBuffer.wrap(decompressed.toByteArray()))
         } catch (e: Exception) {
             decompileC1Cob(buffer)
@@ -33,14 +24,14 @@ object CobDecompiler {
     }
 
 
-    private fun decompileC1Cob(buffer: ByteBuffer): List<CobBlock> {
+    private fun decompileC1Cob(buffer: ByteBuffer): CobFileData.C1CobData {
         val version = buffer.uInt8
         if (version > 4)
             throw Exception("Invalid COB file")
-        return listOf(buffer.readC1Cob())
+        return CobFileData.C1CobData(buffer.readC1Cob())
     }
 
-    private fun decompileC2Cob(buffer: ByteBuffer): List<CobBlock> {
+    private fun decompileC2Cob(buffer: ByteBuffer): CobFileData.C2CobData {
         val header = buffer.cString(4)
         if (header != "cob2")
             throw Exception("Invalid C2 Cob file")
@@ -50,7 +41,7 @@ object CobDecompiler {
                     ?: break
             blocks.add(block)
         }
-        return blocks
+        return CobFileData.C2CobData(blocks)
     }
 
 }
