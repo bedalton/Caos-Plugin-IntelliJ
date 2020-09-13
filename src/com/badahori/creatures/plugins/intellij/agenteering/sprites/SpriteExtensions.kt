15a85845e93fca05edaf87172034f6f495a3f241
daniel <dsbadal@gmail.com>
2020-09-13 13:02:43 -0700

    Add: flip sprite method

--- src/com/badahori/creatures/plugins/intellij/agenteering/sprites/SpriteExtensions.kt
+++ src/com/badahori/creatures/plugins/intellij/agenteering/sprites/SpriteExtensions.kt
@@ -1,12 +1,13 @@
 package com.badahori.creatures.plugins.intellij.agenteering.sprites
 
+import com.intellij.util.ui.UIUtil
 import java.awt.geom.AffineTransform
 import java.awt.image.BufferedImage
 
 
 fun BufferedImage.createTransformed(at: AffineTransform) : BufferedImage {
-    val newImage = BufferedImage(
-            getWidth(), getHeight(),
+    val newImage = UIUtil.createImage(
+            width, height,
             BufferedImage.TYPE_INT_ARGB)
     val g = newImage.createGraphics()
     g.transform(at)
@@ -15,9 +16,15 @@ fun BufferedImage.createTransformed(at: AffineTransform) : BufferedImage {
     return newImage
 }
 
-fun BufferedImage.flip() : BufferedImage? {
+fun BufferedImage.flipVertical() : BufferedImage? {
     val at = AffineTransform()
     at.concatenate(AffineTransform.getScaleInstance(1.0, -1.0))
+    at.concatenate(AffineTransform.getTranslateInstance(0.0, -height.toDouble()))
+    return createTransformed(at)
+}
+fun BufferedImage.flipHorizontal() : BufferedImage? {
+    val at = AffineTransform()
+    at.concatenate(AffineTransform.getScaleInstance(-1.0, 1.0))
     at.concatenate(AffineTransform.getTranslateInstance(0.0, -height.toDouble()))
     return createTransformed(at)
 }
