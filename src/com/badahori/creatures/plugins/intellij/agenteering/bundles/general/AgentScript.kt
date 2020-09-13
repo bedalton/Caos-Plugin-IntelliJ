ac75870c44c8a84bc2ca5f46b7370d702397eac1
daniel <dsbadal@gmail.com>
2020-09-13 13:47:08 -0700

    add COB file script container class

--- src/com/badahori/creatures/plugins/intellij/agenteering/bundles/general/AgentScript.kt
+++ src/com/badahori/creatures/plugins/intellij/agenteering/bundles/general/AgentScript.kt
@@ -3,45 +3,44 @@ package com.badahori.creatures.plugins.intellij.agenteering.bundles.general
 import com.badahori.creatures.plugins.intellij.agenteering.caos.lang.CaosScriptFile
 import com.badahori.creatures.plugins.intellij.agenteering.caos.lang.CaosScriptLanguage
 import com.badahori.creatures.plugins.intellij.agenteering.caos.lang.CaosVariant
-import com.badahori.creatures.plugins.intellij.agenteering.caos.parser.CaosScriptParserDefinition
 import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.CaosScriptEventScript
-import com.intellij.lang.Language
+import com.badahori.creatures.plugins.intellij.agenteering.vfs.CaosVirtualFile
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiFileFactory
 import com.intellij.psi.PsiManager
-import com.intellij.psi.SingleRootFileViewProvider
 import com.intellij.psi.util.PsiTreeUtil
 import java.util.*
 
 
-open class AgentScript(val code: String, val scriptName: String, val type:AgentScriptType) {
-    class EventScript(val family:Int, val genus:Int, val species:Int, val eventNumber:Int, code: String) : AgentScript(code, "$family $genus $species $eventNumber", AgentScriptType.EVENT) {
+open class AgentScript(val code: String, val scriptName: String, val type: AgentScriptType) {
+    class EventScript(val family: Int, val genus: Int, val species: Int, val eventNumber: Int, code: String) : AgentScript(code, "$family $genus $species $eventNumber", AgentScriptType.EVENT) {
         companion object {
-            fun parse(project:Project, variant:CaosVariant, code:String) : AgentScript {
+            fun parse(project: Project, variant: CaosVariant, index: Int, code: String): AgentScript {
                 val uuid = UUID.randomUUID().toString()
                 val psiFile = PsiFileFactory.getInstance(project)
                         .createFileFromText("$uuid.cos", CaosScriptLanguage, code) as CaosScriptFile
                 psiFile.variant = variant
                 val eventScript = PsiTreeUtil.collectElementsOfType(psiFile, CaosScriptEventScript::class.java)
                         .firstOrNull()
-                        ?: return AgentScript(code, "Script", AgentScriptType.OBJECT)
+                        ?: return AgentScript(code, "Script $index", AgentScriptType.OBJECT)
                 return EventScript(eventScript.family, eventScript.genus, eventScript.species, eventScript.eventNumber, code)
             }
         }
     }
-    class InstallScript(code:String, scriptName:String = "InstallScript") : AgentScript(code, scriptName, AgentScriptType.INSTALL)
-    class RemovalScript(code:String, scriptName:String = "InstallScript") : AgentScript(code, scriptName, AgentScriptType.REMOVAL)
-    fun toCaosFile(project: Project, caosVariant: CaosVariant): CaosScriptFile {
-        val psiFile = PsiFileFactory.getInstance(project)
-                .createFileFromText("$scriptName.cos", CaosScriptLanguage, code) as CaosScriptFile
+
+    class InstallScript(code: String, scriptName: String = "InstallScript") : AgentScript(code, scriptName, AgentScriptType.INSTALL)
+    class RemovalScript(code: String, scriptName: String = "InstallScript") : AgentScript(code, scriptName, AgentScriptType.REMOVAL)
+
+    fun toCaosFile(project: Project, cobPath: CaosVirtualFile, caosVariant: CaosVariant): CaosScriptFile {
+        val file = CaosVirtualFile("$scriptName.cos", code, false).apply {
+            cobPath.addChild(this)
+            this.variant = caosVariant
+            isWritable = true
+        }
+        val psiFile = (PsiManager.getInstance(project).findFile(file) as? CaosScriptFile)
+                ?: PsiFileFactory.getInstance(project)
+                        .createFileFromText("$scriptName.cos", CaosScriptLanguage, code) as CaosScriptFile
         psiFile.variant = caosVariant
-        /*val provider = object : SingleRootFileViewProvider(PsiManager.getInstance(project),
-                psiFile.virtualFile,
-                false) {
-            override fun supportsIncrementalReparse(rootLanguage: Language): Boolean {
-                return false
-            }
-        }*/
         return psiFile
     }
 }
