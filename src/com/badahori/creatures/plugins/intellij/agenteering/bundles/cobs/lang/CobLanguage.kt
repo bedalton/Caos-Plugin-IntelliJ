c9029b6f66a7a7f1022726fc674e539abd4c5aa8
daniel <dsbadal@gmail.com>
2020-09-13 13:45:41 -0700

    Remove unneeded CobLanguage file

--- src/com/badahori/creatures/plugins/intellij/agenteering/bundles/cobs/lang/CobLanguage.kt
+++
@@ -1,7 +0,0 @@
-package com.badahori.creatures.plugins.intellij.agenteering.bundles.cobs.lang
-
-import com.intellij.lang.Language
-import com.intellij.openapi.fileTypes.LanguageFileType
-
-
-object CobLanguage : Language("CreaturesCOB")
