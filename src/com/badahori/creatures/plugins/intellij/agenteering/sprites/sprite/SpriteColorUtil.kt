3b22df04013bfdad72a42abf36149219a39c462c
daniel <dsbadal@gmail.com>
2020-09-15 08:25:06 -0700

    clarify operators on getColor pixel int parsing

--- src/com/badahori/creatures/plugins/intellij/agenteering/sprites/sprite/SpriteColorUtil.kt
+++ src/com/badahori/creatures/plugins/intellij/agenteering/sprites/sprite/SpriteColorUtil.kt
@@ -4,15 +4,15 @@ object SpriteColorUtil {
     val solid = intArrayOf(255,255,255,255)
     val transparent = intArrayOf(0,0,0,0)
     fun getColor(pixel:Int, encoding:ColorEncoding) : IntArray {
-        return if (encoding == ColorEncoding.x565) {
-            val red = pixel and 0xF800 shr 8
-            val green = pixel and 0x07E0 shr 3
-            val blue = pixel and 0x001F shl 3
+        return if (encoding == ColorEncoding.X_565) {
+            val red = (pixel and 0xF800) shr 8
+            val green = (pixel and 0x07E0) shr 3
+            val blue = (pixel and 0x001F) shl 3
             intArrayOf(red, green, blue, 0)
         } else {
-            val red = pixel and 0x7C00 shr 7
-            val green = pixel and 0x03E0 shr 2
-            val blue = pixel and 0x001F shl 3
+            val red = (pixel and 0x7C00) shr 7
+            val green = (pixel and 0x03E0) shr 2
+            val blue = (pixel and 0x001F) shl 3
             intArrayOf(red, green, blue, 0)
         }
     }
@@ -20,6 +20,6 @@ object SpriteColorUtil {
 
 
 enum class ColorEncoding {
-    x555,
-    x565
+    X_555,
+    X_565
 }
