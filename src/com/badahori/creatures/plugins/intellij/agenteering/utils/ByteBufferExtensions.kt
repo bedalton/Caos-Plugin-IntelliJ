10eb228453560b583d69eaa0de25aa965a113853
daniel <dsbadal@gmail.com>
2020-09-20 12:41:17 -0700

    Update ByteBufferExtensions.kt

--- src/com/badahori/creatures/plugins/intellij/agenteering/utils/ByteBufferExtensions.kt
+++ src/com/badahori/creatures/plugins/intellij/agenteering/utils/ByteBufferExtensions.kt
@@ -6,58 +6,105 @@ import java.nio.ByteBuffer
 import java.nio.ByteOrder
 
 
-private fun byte2int(b: Byte): Int {
+private fun byteToUInt(b: Byte): Int {
     return if (b < 0) b + 256 else b.toInt()
 }
 
 
-val ByteBuffer.uInt8: Int
-    get() = byte2int(get())
+val ByteBuffer.byte :Int get() {
+    return get().toInt()
+}
 
+val ByteBuffer.int8:Int get() = get().toInt()
+
+val ByteBuffer.uInt8: Int
+    get() = byte and 0xFF
+
+val ByteBuffer.int16: Int
+    get() {
+        return short.toInt()
+    }
+
+val ByteBuffer.int16BE: Int get() {
+    val currentEncoding = order()
+    order(ByteOrder.BIG_ENDIAN)
+    val value = short.toInt()
+    order(currentEncoding)
+    return value
+}
+
+val ByteBuffer.int16LE: Int get() {
+    val currentEncoding = order()
+    order(ByteOrder.LITTLE_ENDIAN)
+    val value = short.toInt()
+    order(currentEncoding)
+    return value
+}
 
 val ByteBuffer.uInt16: Int
     get() {
-        var result = 0
-        result += byte2int(get()) shl 8
-        result += byte2int(get())
-        return result
+        return byteToUInt(get()) + (byteToUInt(get()) shl 8)
     }
 
-val ByteBuffer.uInt16BE:Int get() {
-    var result = 0
-    result += byte2int(get())
-    result += byte2int(get()) shl 8
-    return result
+val ByteBuffer.uInt16LE: Int get() {
+    val currentEncoding = order()
+    order(ByteOrder.LITTLE_ENDIAN)
+    val value = (get().toInt() shl 8) + get()
+    order(currentEncoding)
+    return value
 }
 
-val ByteBuffer.uInt32BE: Long get() {
-    val ch1: Long = uInt8.toLong()
-    val ch2: Long = uInt8.toLong()
-    val ch3: Long = uInt8.toLong()
-    val ch4: Long = uInt8.toLong()
-    return (ch4 shl 24) + (ch3 shl 16) + (ch2 shl 8) + (ch1 shl 0)
+val ByteBuffer.uInt16BE: Int get() {
+    val currentEncoding = order()
+    order(ByteOrder.BIG_ENDIAN)
+    val value = short.toInt() and 0xffff
+    order(currentEncoding)
+    return value
 }
+
+val ByteBuffer.int32:Int get() = int
 
 val ByteBuffer.uInt32: Long
     get() {
-        var i: Long = int.toLong()
-        if (i < 0) {
-            i += 1L shl 32
-        }
-        return i
+        return int.toLong() and 0xffffffffL
     }
 
-val ByteBuffer.uInt24: Int get() {
-    var result = 0
-    result += uInt16 shl 8
-    result += byte2int(get())
-    return result
+fun ByteBuffer.cString(length: Int) : String {
+    return bytes(length).joinToString("") { it.toChar()+"" }
+}
+fun ByteBuffer.cString(length: Long) : String {
+    return bytes(length).joinToString("") { it.toChar()+"" }
 }
 
-private fun ByteBuffer.getBytes(length: Int): List<Byte> {
-    return (0..length).map {
-        get()
+val ByteBuffer.cString : String get() {
+    val stringBuilder = StringBuilder()
+    while(true) {
+        try {
+            val byte = get()
+            if (byte == 0.toByte()) {
+                break
+            }
+            stringBuilder.append(byte.toChar())
+        } catch(e:Exception) { break }
+
     }
+    return stringBuilder.toString()
+}
+
+fun ByteBuffer.bytes(length: Int): ByteArray {
+    return (0 until length).map {
+        get()
+    }.toByteArray()
+}
+
+fun ByteBuffer.bytes(length: Long): ByteArray {
+    return (0 until length).map {
+        get()
+    }.toByteArray()
+}
+
+fun ByteBuffer.skip(length: Int) {
+    bytes(length)
 }
 
 fun ByteBuffer.writeUInt64(u: Long) {
@@ -80,7 +127,7 @@ fun ByteBuffer.writeUInt32BE(u: Long) {
 }
 
 
-fun ByteBuffer.writeUInt24(iIn:Int) {
+fun ByteBuffer.writeUInt24(iIn: Int) {
     var i = iIn
     i = i and 0xFFFFFF
     this.writeUInt16(i shr 8)
@@ -88,30 +135,27 @@ fun ByteBuffer.writeUInt24(iIn:Int) {
 }
 
 
-fun ByteBuffer.writeUInt16(iIn:Int) {
+fun ByteBuffer.writeUInt16(iIn: Int) {
     var i = iIn
     i = i and 0xFFFF
     this.writeUInt8(i shr 8)
     this.writeUInt8(i and 0xFF)
 }
 
-fun ByteBuffer.writeUInt16BE(iIn:Int) {
+fun ByteBuffer.writeUInt16BE(iIn: Int) {
     var i = iIn
     i = i and 0xFFFF
     this.writeUInt8(i and 0xFF)
     this.writeUInt8(i shr 8)
 }
 
-fun ByteBuffer.writeUInt8(iIn:Int) {
+fun ByteBuffer.writeUInt8(iIn: Int) {
     var i = iIn
     i = i and 0xFF
     this.put(i.toByte())
 }
 
-fun ByteBuffer.littleEndian() {
+fun ByteBuffer.littleEndian() :ByteBuffer {
     order(ByteOrder.LITTLE_ENDIAN)
-}
-
-fun ByteBuffer.bigEndian() {
-    order(ByteOrder.LITTLE_ENDIAN)
+    return this
 }
