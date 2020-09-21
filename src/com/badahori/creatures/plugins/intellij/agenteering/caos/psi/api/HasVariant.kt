289c99c17c6a7dea02bcfa028b4c579fbffa6df5
bedalton <Dsbadal@gmail.com>
2020-09-20 17:28:48 -0700

    update variant handling

---
+++ src/com/badahori/creatures/plugins/intellij/agenteering/caos/psi/api/HasVariant.kt
@@ -0,0 +1,7 @@
+package com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api
+
+import com.badahori.creatures.plugins.intellij.agenteering.caos.lang.CaosVariant
+
+interface HasVariant {
+    var variant:CaosVariant?
+}
