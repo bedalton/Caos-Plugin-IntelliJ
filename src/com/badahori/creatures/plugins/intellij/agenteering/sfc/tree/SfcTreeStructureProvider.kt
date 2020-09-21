b2f3066f87a2ca5805ebec7e43d63820836b5809
bedalton <Dsbadal@gmail.com>
2020-09-21 14:45:43 -0700

    add stubs for SFC tree nodes

--- src/com/badahori/creatures/plugins/intellij/agenteering/sfc/tree/SfcTreeStructureProvider.kt
+++ src/com/badahori/creatures/plugins/intellij/agenteering/sfc/tree/SfcTreeStructureProvider.kt
@@ -1,13 +1,20 @@
 package com.badahori.creatures.plugins.intellij.agenteering.sfc.tree
 
+import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.util.LOGGER
 import com.intellij.ide.projectView.TreeStructureProvider
 import com.intellij.ide.projectView.ViewSettings
 import com.intellij.ide.util.treeView.AbstractTreeNode
+import com.intellij.openapi.vfs.VirtualFile
 
 class SfcTreeStructureProvider : TreeStructureProvider{
     override fun modify(node: AbstractTreeNode<*>, children: MutableCollection<AbstractTreeNode<Any>>, settings: ViewSettings?): MutableCollection<AbstractTreeNode<Any>> {
         return children.map { child ->
-            when (child.value) {
+            LOGGER.info("Tree Node: ${child.value}")
+            when (val value = child.value) {
+                is VirtualFile -> {
+                    LOGGER.info("Node is virtual file: ${value.name}")
+                    child
+                }
                 else -> child
             }
         }.toMutableList()
