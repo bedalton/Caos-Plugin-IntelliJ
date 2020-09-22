f8b0e9b6e0bdd585e4115fdc0a210a3e34a6fd5a
bedalton <Dsbadal@gmail.com>
2020-09-21 21:14:39 -0700

    replace Cob script tree node with generic caos script tree node

--- src/com/badahori/creatures/plugins/intellij/agenteering/nodes/CobScriptFileTreeNode.kt
+++ src/com/badahori/creatures/plugins/intellij/agenteering/nodes/CobScriptFileTreeNode.kt
@@ -7,10 +7,7 @@ import com.badahori.creatures.plugins.intellij.agenteering.bundles.cobs.decompil
 import com.badahori.creatures.plugins.intellij.agenteering.bundles.cobs.decompiler.CobBlock.FileBlock.SpriteBlock
 import com.badahori.creatures.plugins.intellij.agenteering.bundles.cobs.decompiler.CobFileData
 import com.badahori.creatures.plugins.intellij.agenteering.bundles.cobs.decompiler.CobToDataObjectDecompiler
-import com.badahori.creatures.plugins.intellij.agenteering.caos.fixes.CaosScriptExpandCommasIntentionAction
-import com.badahori.creatures.plugins.intellij.agenteering.caos.lang.CaosScriptFile
 import com.badahori.creatures.plugins.intellij.agenteering.caos.lang.CaosVariant
-import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.*
 import com.badahori.creatures.plugins.intellij.agenteering.sprites.toPngByteArray
 import com.badahori.creatures.plugins.intellij.agenteering.utils.littleEndian
 import com.badahori.creatures.plugins.intellij.agenteering.utils.orFalse
@@ -19,12 +16,9 @@ import com.badahori.creatures.plugins.intellij.agenteering.vfs.CaosVirtualFileSy
 import com.intellij.ide.projectView.PresentationData
 import com.intellij.ide.util.treeView.AbstractTreeNode
 import com.intellij.ide.util.treeView.smartTree.SortableTreeElement
-import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.pom.Navigatable
 import com.intellij.psi.PsiManager
-import com.intellij.psi.util.PsiTreeUtil
 import org.apache.commons.io.FilenameUtils
 import java.nio.ByteBuffer
 import java.util.concurrent.atomic.AtomicInteger
@@ -59,23 +53,22 @@ class CobFileTreeNode(
     }
 
     private fun getChildren(data: CobFileData.C1CobData): List<AbstractTreeNode<*>> {
-        return getChildren(file.name, data.cobBlock, CaosVariant.C1)
+        return getChildren(data.cobBlock, CaosVariant.C1)
     }
 
     private fun getChildren(data: CobFileData.C2CobData): List<AbstractTreeNode<*>> {
-        val cobFile = file.name
-        return data.let { it.agentBlocks + it.authorBlocks + it.soundFileBlocks + it.spriteFileBlocks }.flatMap { getChildren(cobFile, it, CaosVariant.C2) }
+        return data.let { it.agentBlocks + it.authorBlocks + it.soundFileBlocks + it.spriteFileBlocks }.flatMap { getChildren(it, CaosVariant.C2) }
     }
 
-    private fun getChildren(cobFile: String, block: CobBlock, variant: CaosVariant): List<AbstractTreeNode<*>> {
+    private fun getChildren(block: CobBlock, variant: CaosVariant): List<AbstractTreeNode<*>> {
         return when (block) {
             is SpriteBlock -> listOf(CobSpriteFileTreeNode(nonNullProject, cobVirtualFile, block))
             is SoundBlock -> listOf(SoundFileTreeNode(nonNullProject, cobVirtualFile, block))
-            is AuthorBlock -> listOf(AuthorTreeNode(nonNullProject, cobFile, block))
+            is AuthorBlock -> listOf(AuthorTreeNode(nonNullProject, block))
             is AgentBlock -> {
                 val scripts = listOfNotNull(
-                        block.installScript?.let { CobScriptFileTreeNode(it.toCaosFile(nonNullProject, cobVirtualFile, variant), cobFile, 0, "Install Script") },
-                        block.removalScript?.let { CobScriptFileTreeNode(it.toCaosFile(nonNullProject, cobVirtualFile, variant), cobFile, 0, "Install Script") }
+                        block.installScript?.let { CaosScriptFileTreeNode(it.toCaosFile(nonNullProject, cobVirtualFile, variant), 0, "Install Script") },
+                        block.removalScript?.let { CaosScriptFileTreeNode(it.toCaosFile(nonNullProject, cobVirtualFile, variant), 0, "Install Script") }
                 )
                 val previews:List<AbstractTreeNode<*>> = listOfNotNull(block.image?.let {
                     SpriteImageTreeNode(
@@ -86,7 +79,7 @@ class CobFileTreeNode(
                             )
                 })
                 previews + scripts + block.eventScripts.mapIndexed { index, script ->
-                    CobScriptFileTreeNode(script.toCaosFile(nonNullProject, cobVirtualFile, variant), cobFile, index)
+                    CaosScriptFileTreeNode(script.toCaosFile(nonNullProject, cobVirtualFile, variant), index)
                 }
             }
             is CobBlock.UnknownCobBlock -> emptyList()
@@ -105,7 +98,7 @@ class CobFileTreeNode(
     }
 }
 
-internal class AuthorTreeNode(project: Project, private val enclosingCobFileName: String, block: AuthorBlock)
+internal class AuthorTreeNode(project: Project, block: AuthorBlock)
     : AbstractTreeNode<AuthorBlock>(project, block), SortableTreeElement {
     override fun getChildren(): List<AbstractTreeNode<*>> = emptyList()
     override fun navigate(p0: Boolean) {}
@@ -203,147 +196,6 @@ internal class SoundFileTreeNode(project: Project, private val enclosingCob: Cao
     }
     override fun getWeight(): Int = 2
 
-}
-
-internal class CobScriptFileTreeNode(
-        private val caosFile: CaosScriptFile,
-        private val enclosingCobFileName: String,
-        private val scriptIndex: Int,
-        private val presentableTextIn: String? = null,
-        private val alwaysLeaf: Boolean = PsiTreeUtil.collectElementsOfType(caosFile, CaosScriptScriptElement::class.java).size < 2
-) : AbstractTreeNode<CaosScriptFile>(caosFile.project, caosFile), SortableTreeElement  {
-
-    override fun isAlwaysLeaf(): Boolean {
-        return alwaysLeaf
-    }
-
-    override fun navigate(requestFocus: Boolean) {
-        quickFormat(caosFile)
-        caosFile.navigate(requestFocus)
-    }
-
-    override fun canNavigate(): Boolean {
-        return caosFile.canNavigateToSource()
-    }
-
-    override fun canNavigateToSource(): Boolean {
-        return caosFile.canNavigateToSource()
-    }
-
-    private fun getPresentableText(): String {
-        this@CobScriptFileTreeNode.presentableTextIn?.let {
-            return it
-        }
-        val scripts = PsiTreeUtil.collectElementsOfType(caosFile, CaosScriptScriptElement::class.java)
-        if (scripts.size != 1 || scripts.filter { it !is CaosScriptMacro }.size != 1) {
-            return "Script $scriptIndex"
-        }
-        return when (val script = scripts.firstOrNull { it !is CaosScriptMacro }) {
-            is CaosScriptEventScript -> "${script.family} ${script.genus} ${script.species} ${script.eventNumber}"
-            is CaosScriptInstallScript -> "Install Script"
-            is CaosScriptRemovalScript -> "Removal Script"
-            else -> "Script $scriptIndex"
-        }
-    }
-
-    override fun getChildren(): List<AbstractTreeNode<*>> {
-        val scripts = PsiTreeUtil.collectElementsOfType(caosFile, CaosScriptScriptElement::class.java)
-        if (scripts.size != 1 || scripts.filter { it !is CaosScriptMacro }.size != 1) {
-            return scripts.map {
-                SubScriptLeafNode(it, enclosingCobFileName)
-            }
-        }
-        return emptyList()
-    }
-
-    override fun update(presentationData: PresentationData) {
-        presentationData.presentableText = getPresentableText()
-        presentationData.locationString = "(Decompiled)"
-    }
-
-    override fun getAlphaSortKey(): String {
-        return "$weight"
-    }
-
-    override fun getWeight(): Int {
-        val script = PsiTreeUtil.collectElementsOfType(caosFile, CaosScriptScriptElement::class.java)
-                .firstOrNull { it !is CaosScriptMacro }
-        return when (script) {
-            is CaosScriptInstallScript -> 7
-            is CaosScriptRemovalScript -> 8
-            else -> 9
-        }
-    }
-}
-
-
-internal class SubScriptLeafNode(
-        private val script: CaosScriptScriptElement,
-        private val enclosingCobFileName: String
-) : AbstractTreeNode<CaosScriptScriptElement>(script.project, script), SortableTreeElement {
-
-    private val text by lazy {
-        when (script) {
-            is CaosScriptEventScript -> "${script.family} ${script.genus} ${script.species} ${script.eventNumber}"
-            is CaosScriptInstallScript -> "Install Script"
-            is CaosScriptRemovalScript -> "Removal Script"
-            else -> "Body Script"
-        }
-    }
-
-    override fun getChildren(): List<AbstractTreeNode<*>> {
-        return emptyList()
-    }
-
-    override fun navigate(requestFocus: Boolean) {
-        virtualFile?.isWritable = false
-        (script as? Navigatable)?.navigate(requestFocus)
-    }
-
-    override fun getVirtualFile(): VirtualFile? {
-        return (script.containingFile ?: script.originalElement.containingFile).virtualFile
-    }
-
-    override fun canNavigate(): Boolean = (script as? Navigatable)?.canNavigate().orFalse()
-
-    override fun canNavigateToSource(): Boolean = (script as? Navigatable)?.canNavigateToSource().orFalse()
-
-    override fun getAlphaSortKey(): String {
-        return when (script) {
-            is CaosScriptEventScript -> "x" + listOf(script.family, script.genus, script.species, script.eventNumber)
-                    .joinToString("|") { it.toString().padStart(8, '0') }
-            is CaosScriptInstallScript -> "0"
-            is CaosScriptRemovalScript -> "1"
-            else -> "z"
-        }
-    }
-
-    override fun update(presentationData: PresentationData) {
-        presentationData.presentableText = text
-        presentationData.locationString = enclosingCobFileName
-    }
-
-    override fun getWeight(): Int = when(script) {
-        is CaosScriptInstallScript -> 7
-        is CaosScriptRemovalScript -> 8
-        else -> 9
-    }
-}
-
-internal fun quickFormat(caosFile: CaosScriptFile) {
-    val variant = caosFile.variant ?: CaosVariant.C1
-    val command: WriteCommandAction<Void?> = object : WriteCommandAction.Simple<Void?>(caosFile.project, caosFile) {
-        override fun run() {
-            caosFile.virtualFile?.isWritable = true
-            caosFile.variant = variant
-            CaosScriptExpandCommasIntentionAction.invoke(project, caosFile)
-            caosFile.variant = variant
-            /*CodeStyleManager.getInstance(project).reformat(caosFile, false)
-            caosFile.variant = variant*/
-            caosFile.virtualFile?.isWritable = false
-        }
-    }
-    command.execute()
 }
 
 private val decompiledId = AtomicInteger(0)
