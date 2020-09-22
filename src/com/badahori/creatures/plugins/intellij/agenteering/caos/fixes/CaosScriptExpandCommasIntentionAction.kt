7513c18558da293c789bb765512443939823069d
bedalton <3941960+bedalton@users.noreply.github.com>
2020-09-21 20:15:53 -0700

    resolve git merge problems

--- src/com/badahori/creatures/plugins/intellij/agenteering/caos/fixes/CaosScriptExpandCommasIntentionAction.kt
+++ src/com/badahori/creatures/plugins/intellij/agenteering/caos/fixes/CaosScriptExpandCommasIntentionAction.kt
@@ -1,30 +1,19 @@
 package com.badahori.creatures.plugins.intellij.agenteering.caos.fixes
 
-<<<<<<< Updated upstream
-=======
 import com.badahori.creatures.plugins.intellij.agenteering.caos.lang.CaosBundle
 import com.badahori.creatures.plugins.intellij.agenteering.caos.lang.CaosScriptFile
-import com.badahori.creatures.plugins.intellij.agenteering.caos.lang.CaosVariant
-import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.CaosScriptSpaceLikeOrNewline
 import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.util.CaosScriptPsiElementFactory
 import com.badahori.creatures.plugins.intellij.agenteering.utils.document
->>>>>>> Stashed changes
 import com.intellij.codeInsight.intention.IntentionAction
+import com.intellij.codeInspection.LocalQuickFix
+import com.intellij.codeInspection.ProblemDescriptor
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
+import com.intellij.psi.codeStyle.CodeStyleManager
 import com.intellij.psi.util.PsiTreeUtil
-<<<<<<< Updated upstream
-import com.badahori.creatures.plugins.intellij.agenteering.caos.lang.CaosBundle
-import com.badahori.creatures.plugins.intellij.agenteering.caos.lang.CaosScriptFile
-import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.util.CaosScriptPsiElementFactory
-import com.badahori.creatures.plugins.intellij.agenteering.utils.document
-import com.intellij.codeInspection.LocalQuickFix
-import com.intellij.codeInspection.ProblemDescriptor
-=======
->>>>>>> Stashed changes
 
 object CaosScriptExpandCommasIntentionAction : IntentionAction, LocalQuickFix {
     override fun startInWriteAction(): Boolean = true
@@ -48,41 +37,24 @@ object CaosScriptExpandCommasIntentionAction : IntentionAction, LocalQuickFix {
                 ?: return
         invoke(project, file)
     }
-
-<<<<<<< Updated upstream
-    private fun invoke(project: Project, fileIn: PsiFile?) {
-        val file = fileIn ?: return
-        var document = PsiDocumentManager.getInstance(project).getCachedDocument(file) ?: fileIn.document
-=======
     fun invoke(project: Project, fileIn: PsiFile?) {
-
         val file = fileIn as? CaosScriptFile ?: return
-        val variant = file.variant
         var document = fileIn.document ?: PsiDocumentManager.getInstance(project).getCachedDocument(file)
->>>>>>> Stashed changes
         if (document != null) {
             PsiDocumentManager.getInstance(project).commitDocument(document)
         }
         val newLines = PsiTreeUtil.collectElementsOfType(file, com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.CaosScriptSpaceLikeOrNewline::class.java)
         for (newLine in newLines) {
-            replaceIfBlankOrComma(variant, newLine)
+            replaceIfBlankOrComma(newLine)
         }
         document = file.document ?: PsiDocumentManager.getInstance(project).getCachedDocument(file)
         document?.let {
             PsiDocumentManager.getInstance(project).doPostponedOperationsAndUnblockDocument(it)
         }
-<<<<<<< Updated upstream
         CodeStyleManager.getInstance(project).reformat(fileIn, true)
     }
 
     private fun replaceIfBlankOrComma(next:PsiElement?) {
-=======
-        //CodeStyleManager.getInstance(project).reformat(file, true)
-        //file.variant = variant
-    }
-
-    private fun replaceIfBlankOrComma(variant:CaosVariant?, next: PsiElement?) {
->>>>>>> Stashed changes
         if (next == null)
             return
         if (next.text.trim(' ', '\n', ',').isEmpty() || next.text == ",")
