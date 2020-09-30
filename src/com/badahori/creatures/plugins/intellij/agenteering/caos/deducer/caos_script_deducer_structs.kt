3b3a9d8298394c364a34ada533a8d28042825f77
daniel <dsbadal@gmail.com>
2020-09-30 09:31:40 -0700

    re-organize global psi util functions

--- src/com/badahori/creatures/plugins/intellij/agenteering/caos/deducer/caos_script_deducer_structs.kt
+++ src/com/badahori/creatures/plugins/intellij/agenteering/caos/deducer/caos_script_deducer_structs.kt
@@ -3,52 +3,6 @@ package com.badahori.creatures.plugins.intellij.agenteering.caos.deducer
 import com.intellij.openapi.util.TextRange
 import com.badahori.creatures.plugins.intellij.agenteering.caos.lang.CaosScriptFile
 
-
-data class CaosScope(val range:TextRange, val blockType:CaosScriptBlockType, val enclosingScope:List<CaosScope>) {
-    val startOffset:Int get() = range.startOffset
-    val endOffset:Int get() = range.endOffset
-}
-
-fun CaosScope?.sharesScope(otherScopeIn: CaosScope?) : Boolean {
-    if (this == null) {
-        return otherScopeIn == null
-    }
-    if (otherScopeIn == null)
-        return false
-    if (this == otherScopeIn) {
-        return true
-    }
-    val thisEnclosingScopes = enclosingScope
-    val otherEnclosingScope = otherScopeIn.enclosingScope
-    val longestScope:List<CaosScope>
-    val otherScope:List<CaosScope>
-    if (thisEnclosingScopes.size > otherEnclosingScope.size) {
-        longestScope = thisEnclosingScopes
-        otherScope = otherEnclosingScope
-    } else {
-        longestScope = otherEnclosingScope
-        otherScope = thisEnclosingScopes
-    }
-    for(i in longestScope.indices) {
-        val parentScope = longestScope[i]
-        val otherParentScope = otherScope.getOrNull(i)
-                ?: return true
-        if (parentScope != otherParentScope) {
-            return when (parentScope.blockType) {
-                CaosScriptBlockType.DOIF -> otherParentScope.blockType == CaosScriptBlockType.DOIF
-                CaosScriptBlockType.ELIF -> otherParentScope.blockType == CaosScriptBlockType.ELIF
-                CaosScriptBlockType.ELSE -> otherParentScope.blockType == CaosScriptBlockType.ELSE
-                else -> true
-            }
-        }
-    }
-    return true
-}
-
-fun rootScope(file:CaosScriptFile) : CaosScope {
-    return CaosScope(file.textRange, CaosScriptBlockType.MACRO, emptyList())
-}
-
 enum class CaosOp(val value:Int) {
     SETV(0),
     ORRV(1),
