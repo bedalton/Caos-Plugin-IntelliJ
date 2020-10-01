db6f6d87f1a183b1456a9c73558b5e9c1280a648
bedalton <3941960+bedalton@users.noreply.github.com>
2020-10-01 14:27:24 -0700

    add quick format method

--- src/com/badahori/creatures/plugins/intellij/agenteering/caos/lang/CaosScriptFile.kt
+++ src/com/badahori/creatures/plugins/intellij/agenteering/caos/lang/CaosScriptFile.kt
@@ -1,24 +1,31 @@
 package com.badahori.creatures.plugins.intellij.agenteering.caos.lang
 
-import com.badahori.creatures.plugins.intellij.agenteering.bundles.cobs.lang.CobFileType
+import com.badahori.creatures.plugins.intellij.agenteering.caos.fixes.CaosScriptExpandCommasIntentionAction
 import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.HasVariant
-import com.badahori.creatures.plugins.intellij.agenteering.caos.settings.CaosScriptProjectSettings
+import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.util.LOGGER
 import com.badahori.creatures.plugins.intellij.agenteering.caos.stubs.api.CaosScriptFileStub
 import com.badahori.creatures.plugins.intellij.agenteering.utils.VariantFilePropertyPusher
+import com.badahori.creatures.plugins.intellij.agenteering.utils.invokeLater
+import com.badahori.creatures.plugins.intellij.agenteering.utils.orFalse
 import com.badahori.creatures.plugins.intellij.agenteering.utils.variant
 import com.badahori.creatures.plugins.intellij.agenteering.vfs.CaosVirtualFile
 import com.intellij.extapi.psi.PsiFileBase
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.fileTypes.FileType
 import com.intellij.openapi.module.Module
+import com.intellij.openapi.project.DumbService
 import com.intellij.openapi.roots.ProjectRootManager
+import com.intellij.openapi.util.Computable
 import com.intellij.openapi.util.Key
 import com.intellij.psi.FileViewProvider
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.intellij.psi.util.PsiTreeUtil
+import java.util.concurrent.atomic.AtomicBoolean
 
 class CaosScriptFile(viewProvider: FileViewProvider)
     : PsiFileBase(viewProvider, CaosScriptLanguage), HasVariant {
+    private val didFormatInitial = AtomicBoolean(false)
     override var variant: CaosVariant?
         get() {
             getUserData(VariantUserDataKey)?.let {
@@ -64,9 +71,57 @@ class CaosScriptFile(viewProvider: FileViewProvider)
         return "Caos Script"
     }
 
+    fun quickFormat() {
+        Companion.quickFormat(this)
+    }
+
     companion object {
         @JvmStatic
         val VariantUserDataKey = Key<CaosVariant?>("com.badahori.creatures.plugins.intellij.agenteering.caos.SCRIPT_VARIANT_KEY")
+
+        fun quickFormat(caosFile: CaosScriptFile) {
+            if (caosFile.didFormatInitial.getAndSet(true))
+                return
+            val project = caosFile.project
+            val application = ApplicationManager.getApplication()
+            when {
+                application.isWriteAccessAllowed -> {
+                    runQuickFormatInWriteAction(caosFile)
+                }
+                DumbService.isDumb(project) -> {
+                    if (!application.isDispatchThread) {
+                        DumbService.getInstance(project).runWhenSmart {
+                            application.runWriteAction {
+                                runQuickFormatInWriteAction(caosFile)
+                            }
+                        }
+                    } else {
+                        DumbService.getInstance(project).runWhenSmart {
+                            application.runWriteAction {
+                                runQuickFormatInWriteAction(caosFile)
+                            }
+                        }
+                    }
+                }
+                application.isDispatchThread -> {
+                    application.runWriteAction(Computable {
+                        runQuickFormatInWriteAction(caosFile)
+                    })
+                }
+                else -> {
+                    application.invokeLater {
+                        application.runWriteAction {
+                            runQuickFormatInWriteAction(caosFile)
+                        }
+                    }
+                }
+            }
+        }
+
+        private fun runQuickFormatInWriteAction(caosFile: CaosScriptFile) {
+            if (caosFile.virtualFile?.apply { isWritable = true }?.isWritable.orFalse())
+                CaosScriptExpandCommasIntentionAction.invoke(caosFile.project, caosFile)
+        }
 
     }
 }
@@ -78,8 +133,10 @@ val PsiFile.module: Module?
 
 val RUN_INSPECTIONS_KEY = Key<Boolean?>("com.badahori.creatures.plugins.intellij.agenteering.caos.RUN_INSPECTIONS")
 
-var PsiFile.runInspections:Boolean get() {
-    return getUserData(RUN_INSPECTIONS_KEY) ?: true
-} set(value) {
-    putUserData(RUN_INSPECTIONS_KEY, value)
-}
+var PsiFile.runInspections: Boolean
+    get() {
+        return getUserData(RUN_INSPECTIONS_KEY) ?: true
+    }
+    set(value) {
+        putUserData(RUN_INSPECTIONS_KEY, value)
+    }
