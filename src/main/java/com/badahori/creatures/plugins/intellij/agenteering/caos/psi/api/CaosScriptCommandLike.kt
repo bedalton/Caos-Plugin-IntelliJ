331e829ac99fbabc4e644d57c593b07f0d2e49c9
daniel <dsbadal@gmail.com>
2020-11-05 14:25:23 -0800

    remove unecessary commandDefinition value

--- src/main/java/com/badahori/creatures/plugins/intellij/agenteering/caos/psi/api/CaosScriptCommandLike.kt
+++ src/main/java/com/badahori/creatures/plugins/intellij/agenteering/caos/psi/api/CaosScriptCommandLike.kt
@@ -2,9 +2,12 @@ package com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api
 
 import com.badahori.creatures.plugins.intellij.agenteering.caos.libs.CaosCommand
 
+/**
+ * Represents command like elements.
+ * This encompases both commands with arguments and command keyword elements
+ */
 interface CaosScriptCommandLike : CaosScriptCompositeElement {
     val commandString:String
-    val commandDefinition: CaosCommand?
     @JvmDefault
     val commandToken:CaosScriptIsCommandToken? get() = when (this) {
         is CaosScriptIsCommandToken -> this
