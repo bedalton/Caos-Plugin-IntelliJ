cf4a75e53d465fa96ed3dbde7da9feceb2a2fdfa
daniel <dsbadal@gmail.com>
2020-11-05 14:25:46 -0800

    fill in token rvalue mixin with new missing values

--- src/main/java/com/badahori/creatures/plugins/intellij/agenteering/caos/psi/impl/CaosScriptTokenRvalueMixin.kt
+++ src/main/java/com/badahori/creatures/plugins/intellij/agenteering/caos/psi/impl/CaosScriptTokenRvalueMixin.kt
@@ -1,6 +1,7 @@
 package com.badahori.creatures.plugins.intellij.agenteering.caos.psi.impl
 
 import com.badahori.creatures.plugins.intellij.agenteering.caos.deducer.CaosVar
+import com.badahori.creatures.plugins.intellij.agenteering.caos.libs.CaosCommand
 import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.*
 import com.badahori.creatures.plugins.intellij.agenteering.caos.stubs.api.CaosScriptTokenRValueStub
 import com.intellij.lang.ASTNode
@@ -24,9 +25,26 @@ abstract class CaosScriptTokenRvalueMixin : CaosScriptStubBasedElementImpl<CaosS
 
     fun toCaosVar(): CaosVar = CaosVar.CaosLiteral.CaosToken(text)
 
-    val arguments: List<CaosScriptArgument> get() = emptyList()
+    val arguments: List<CaosScriptArgument> by lazy { emptyList() }
 
-    val argumentValues: List<CaosVar> get() = emptyList()
+    val argumentValues: List<CaosVar> by lazy { emptyList() }
 
-    val inferredType: CaosExpressionValueType = CaosExpressionValueType.TOKEN
+    val inferredType: CaosExpressionValueType get() = CaosExpressionValueType.TOKEN
+
+    val quoteStringLiteral:CaosScriptQuoteStringLiteral? get() = null
+
+    val c1String: CaosScriptC1String? get() = null
+
+    val byteString: CaosScriptByteString? get() = null
+
+    val animationString: CaosScriptAnimationString? get() = null
+
+    val number: CaosScriptNumber? get() = null
+
+    val pictDimensionLiteral: CaosScriptPictDimensionLiteral? get() = null
+
+    val incomplete: CaosScriptIncomplete? get() = null
+
+    val commandDefinition:CaosCommand? get() = null
+
 }
