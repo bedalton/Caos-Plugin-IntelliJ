9dadf23c5c91ea3a577b574f52a94605e12a55d4
daniel <dsbadal@gmail.com>
2020-11-09 14:03:46 -0800

    simplify getDescriptiveText to use CaosLib. son

--- src/main/java/com/badahori/creatures/plugins/intellij/agenteering/caos/documentation/CaosScriptPresentationUtil.kt
+++ src/main/java/com/badahori/creatures/plugins/intellij/agenteering/caos/documentation/CaosScriptPresentationUtil.kt
@@ -1,11 +1,14 @@
 package com.badahori.creatures.plugins.intellij.agenteering.caos.documentation
 
-import com.intellij.navigation.ItemPresentation
+import com.badahori.creatures.plugins.intellij.agenteering.caos.def.psi.api.CaosDefCodeBlock
 import com.badahori.creatures.plugins.intellij.agenteering.caos.def.psi.api.CaosDefCommandDefElement
+import com.badahori.creatures.plugins.intellij.agenteering.caos.def.psi.api.CaosDefCommandWord
+import com.badahori.creatures.plugins.intellij.agenteering.caos.def.psi.api.variants
+import com.badahori.creatures.plugins.intellij.agenteering.caos.libs.CaosLibs
+import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.CaosScriptCommandElement
 import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.CaosScriptIsCommandToken
-import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.util.CaosCommandType
-import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.util.getEnclosingCommandType
-import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.util.getSelfOrParentOfType
+import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.util.*
+import com.intellij.navigation.ItemPresentation
 import icons.CaosScriptIcons
 import javax.swing.Icon
 
@@ -33,15 +36,60 @@ object CaosScriptPresentationUtil {
         }
     }
     fun getDescriptiveText(element:CaosScriptIsCommandToken) : String {
-        val simpleDescription = "command [${element.text}]"
-        val resolved = element
-                .reference
-                .multiResolve(true)
-                .firstOrNull()
-                ?.element
-                ?: return simpleDescription
-        val command = resolved.getSelfOrParentOfType(CaosDefCommandDefElement::class.java)
-                ?: return simpleDescription
-        return command.fullCommand
+        // get command from Command Element parent
+        return (element.parent as? CaosScriptCommandElement)
+                ?.commandDefinition
+                ?.let {commandDefinition ->
+                    return commandDefinition.fullCommandHeader
+                }
+                // Get command from parent command def element
+                ?: (element.parent?.parent as? CaosDefCommandDefElement)
+                        ?.fullCommandHeader
+                // Get command from parent Caos Def Code Block
+                ?: (element.parent as? CaosDefCodeBlock)?.let {
+                    getCommandHeaderFromCodeBlock(element as CaosDefCommandWord)
+                }
+                // Return generic description of command on no matches
+                ?: {
+                    LOGGER.info("Failed to understand Command token ${element.elementType}('${element.text}) for getDescriptiveText with parent ${element.parent?.elementType}(${element.parent?.text})")
+                    "command [${element.text}]"
+                }()
+    }
+
+    private fun getCommandHeaderFromCodeBlock(element:CaosDefCommandWord) : String? {
+        val elementText = element.text
+        val previousWordElement
+                = (element.getPreviousNonEmptyNode(false) as? CaosDefCommandWord)
+        val commandCombinations = mutableListOf<String>()
+        if (previousWordElement != null) {
+            val withPrevious = "${previousWordElement.text} $elementText"
+            (previousWordElement.getPreviousNonEmptyNode(false) as? CaosDefCommandWord)
+                    ?.text
+                    ?.let {previousPrevious ->
+                        commandCombinations.add("$previousPrevious $withPrevious")
+                    }
+
+            commandCombinations.add(withPrevious)
+        }
+        val nextElement
+            = (element.getNextNonEmptySibling(false) as? CaosDefCommandWord)
+        if (nextElement != null) {
+            val withNext = "$elementText ${nextElement.text}"
+            (nextElement.getNextNonEmptySibling(false) as? CaosDefCommandWord)
+                    ?.text
+                    ?.let {nextNext ->
+                        commandCombinations.add("$withNext $nextNext")
+                    }
+            if (previousWordElement != null) {
+                commandCombinations.add("${previousWordElement.text} $withNext")
+            }
+            commandCombinations.add(withNext)
+        }
+        val variants = element.variants.map { it.code }
+        return commandCombinations.mapNotNull {commandString ->
+            CaosLibs.commands(commandString).firstOrNull {command ->
+                command.variants.intersect(variants).isNotEmpty()
+            }
+        }.firstOrNull()?.fullCommandHeader
     }
 }
