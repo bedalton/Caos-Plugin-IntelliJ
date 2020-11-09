0e8fd3dfed6acb7b738ae648899ecdb54d9f94ab
daniel <dsbadal@gmail.com>
2020-11-09 08:55:59 -0800

    move synthetic library to its own file

--- src/main/java/com/badahori/creatures/plugins/intellij/agenteering/caos/project/library/CaosLibraryType.kt
+++ src/main/java/com/badahori/creatures/plugins/intellij/agenteering/caos/project/library/CaosLibraryType.kt
@@ -55,39 +55,3 @@ class CaosLibraryType : LibraryType<DummyLibraryProperties>(LIBRARY) {
         }
     }
 }
-
-@ApiStatus.Experimental
-internal object CaosSyntheticLibrary: SyntheticLibrary(), ItemPresentation {
-
-    val sources:VirtualFile? get() = libraryPath()
-
-    override fun equals(other: Any?): Boolean = other == this
-
-    override fun hashCode(): Int = sources.hashCode()
-    override fun getPresentableText(): String = "CAOS-Std-Lib"
-
-    override fun getLocationString(): String? = null
-
-    override fun getIcon(p0: Boolean): Icon? = CaosScriptIcons.SDK_ICON
-
-    override fun getSourceRoots(): MutableCollection<VirtualFile> {
-        return sources?.let { mutableListOf(it) } ?: mutableListOf()
-    }
-}
-
-/**
- * Gets a path to the plugins store of CaosLibs
- */
-internal fun libraryPath() : VirtualFile? {
-    val libraryPath = CaosFileUtil.getPluginResourceFile(BUNDLE_DEFINITIONS_FOLDER)
-    if (libraryPath == null) {
-        val pluginRoot = CaosFileUtil.PLUGIN_HOME_DIRECTORY
-        if (pluginRoot == null || !pluginRoot.exists()) {
-            LOGGER.severe("Failed to locate bundled caos definition files: Plugin root is invalid")
-        } else {
-            LOGGER.severe("Failed to locate bundled caos definition files: Files in plugin root is <${pluginRoot.children?.map { it.name }}>")
-        }
-        return null
-    }
-    return libraryPath
-}
