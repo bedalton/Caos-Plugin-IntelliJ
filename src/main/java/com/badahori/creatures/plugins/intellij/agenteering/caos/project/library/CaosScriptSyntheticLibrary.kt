c5210579a870b816c5aac8464c2ccb6deadbc08c
daniel <dsbadal@gmail.com>
2020-11-13 08:22:45 -0800

    remove CAOS Def VFS root in synthetic library sources

--- src/main/java/com/badahori/creatures/plugins/intellij/agenteering/caos/project/library/CaosScriptSyntheticLibrary.kt
+++ src/main/java/com/badahori/creatures/plugins/intellij/agenteering/caos/project/library/CaosScriptSyntheticLibrary.kt
@@ -1,5 +1,8 @@
+@file:Suppress("UnstableApiUsage")
+
 package com.badahori.creatures.plugins.intellij.agenteering.caos.project.library
 
+import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.util.LOGGER
 import com.badahori.creatures.plugins.intellij.agenteering.utils.CaosFileUtil
 import com.badahori.creatures.plugins.intellij.agenteering.utils.mutableListOfNotNull
 import com.badahori.creatures.plugins.intellij.agenteering.vfs.CaosVirtualFileSystem
@@ -7,11 +10,11 @@ import com.intellij.navigation.ItemPresentation
 import com.intellij.openapi.roots.SyntheticLibrary
 import com.intellij.openapi.vfs.VirtualFile
 import icons.CaosScriptIcons
-import org.jetbrains.annotations.ApiStatus
+import org.jetbrains.annotations.ApiStatus.Experimental
 import javax.swing.Icon
 
 
-@ApiStatus.Experimental
+@Experimental
 internal object CaosSyntheticLibrary: SyntheticLibrary(), ItemPresentation {
 
     val sources: VirtualFile? get() = libraryPath()
@@ -19,6 +22,7 @@ internal object CaosSyntheticLibrary: SyntheticLibrary(), ItemPresentation {
     override fun equals(other: Any?): Boolean = other == this
 
     override fun hashCode(): Int = sources.hashCode()
+
     override fun getPresentableText(): String = "CAOS-Std-Lib"
 
     override fun getLocationString(): String? = null
@@ -26,13 +30,9 @@ internal object CaosSyntheticLibrary: SyntheticLibrary(), ItemPresentation {
     override fun getIcon(p0: Boolean): Icon? = CaosScriptIcons.SDK_ICON
 
     override fun getSourceRoots(): MutableCollection<VirtualFile> {
-        val vfsLibraryVirtualFile = CaosVirtualFileSystem
-                .instance
-                .getOrCreateRootChildDirectory(BUNDLE_DEFINITIONS_FOLDER)
-        return mutableListOfNotNull(sources, vfsLibraryVirtualFile)
+        return mutableListOfNotNull(sources)
     }
 }
-
 /**
  * Gets a path to the plugins store of CaosLibs
  */
@@ -41,11 +41,22 @@ internal fun libraryPath() : VirtualFile? {
     if (libraryPath == null) {
         val pluginRoot = CaosFileUtil.PLUGIN_HOME_DIRECTORY
         if (pluginRoot == null || !pluginRoot.exists()) {
-            com.badahori.creatures.plugins.intellij.agenteering.caos.psi.util.LOGGER.severe("Failed to locate bundled caos definition files: Plugin root is invalid")
+            LOGGER.severe("Failed to locate bundled caos definition files: Plugin root is invalid")
         } else {
-            com.badahori.creatures.plugins.intellij.agenteering.caos.psi.util.LOGGER.severe("Failed to locate bundled caos definition files: Files in plugin root is <${pluginRoot.children?.map { it.name }}>")
+            LOGGER.severe("Failed to locate bundled caos definition files: Files in plugin root is <${pluginRoot.children?.map { it.name }}>")
         }
         return null
     }
     return libraryPath
 }
+
+/*
+/**
+ * Gets a path to the plugins store of CaosLibs
+ */
+internal fun libraryPath() : VirtualFile? {
+    return CaosVirtualFileSystem
+            .instance
+            .getOrCreateRootChildDirectory(BUNDLE_DEFINITIONS_FOLDER)
+}
+ */
