015d6f5e12a221a124ef4127c6fd5fe9805a4cfe
daniel <dsbadal@gmail.com>
2020-11-19 07:23:44 -0800

    work on highlight var token usages

--- src/main/java/com/badahori/creatures/plugins/intellij/agenteering/caos/references/CaosScriptVarTokenReference.kt
+++ src/main/java/com/badahori/creatures/plugins/intellij/agenteering/caos/references/CaosScriptVarTokenReference.kt
@@ -5,7 +5,9 @@ import com.badahori.creatures.plugins.intellij.agenteering.caos.def.psi.api.Caos
 import com.badahori.creatures.plugins.intellij.agenteering.caos.def.psi.api.CaosDefCommandWord
 import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.CaosScriptVarToken
 import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.impl.variant
-import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.types.CaosScriptVarTokenGroup
+import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.types.CaosScriptVarTokenGroup.*
+import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.util.LOGGER
+import com.badahori.creatures.plugins.intellij.agenteering.utils.nullIfEmpty
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiElementResolveResult
@@ -17,50 +19,77 @@ import com.intellij.psi.ResolveResult
  */
 class CaosScriptVarTokenReference(element: CaosScriptVarToken) : PsiPolyVariantReferenceBase<CaosScriptVarToken>(element, TextRange.create(0, element.text.length), true) {
 
-    private val name:String by lazy {
+    private val name: String by lazy {
         element.text.toUpperCase()
     }
 
-    override fun isReferenceTo(element: PsiElement): Boolean {
-        if (element is CaosDefCommandWord || element is CaosDefCommand) {
-            return when (element.text.toUpperCase()) {
-                "VARX" -> myElement.varGroup == CaosScriptVarTokenGroup.VARx
-                "VAXX" -> myElement.varGroup == CaosScriptVarTokenGroup.VAxx
-                "OBVX" -> myElement.varGroup == CaosScriptVarTokenGroup.OBVx
-                "OVXX" -> myElement.varGroup == CaosScriptVarTokenGroup.OVxx
-                "MVXX" -> myElement.varGroup == CaosScriptVarTokenGroup.MVxx
-                else -> false
-            }
-        }
-        if (element.text.toUpperCase() != name)
-            return false
-        return element.containingFile.isEquivalentTo(myElement.containingFile)
-    }
-/*
-    override fun resolve(): PsiElement? {
-        val key = myElement.varGroup.value
-        val variant = myElement.containingCaosFile?.variant?.let {
-            if (it != CaosVariant.UNKNOWN)
-                it
-            else
-                null
-        } ?: return null
-        return CaosDefCommandElementsByNameIndex
-                .Instance[key, element.project].firstOrNull {
-                    it.isVariant(variant)
+    private val check: (element: PsiElement) -> Boolean by lazy {
+        val variantCheck:(element:PsiElement)->Boolean = myElement.variant?.let { variant ->
+            { element: PsiElement -> element.variant == variant }
+        } ?: { true }
+        val varGroup = myElement.varGroup
+        val varIndex = myElement.varIndex
+        when (varGroup) {
+            VARx, VAxx -> check@{ element ->
+                if (!variantCheck(element)) {
+                    return@check false
                 }
+                if (element is CaosDefCommandWord || element is CaosDefCommand) {
+                    LOGGER.info("Is '$name' reference to '${element.text}' in ${myElement.containingFile.name}")
+                    element.text.toUpperCase().let { it == "VARX" || it == "VAXX" }
+                } else if (element is CaosScriptVarToken) {
+                    LOGGER.info("Is '$name' reference to '${element.text}' in ${myElement.containingFile.name}")
+                    varIndex == element.varIndex && element.varGroup.let { it == VARx || it == VAxx }
+                } else {
+                    false
+                }
+            }
+            OBVx, OVxx -> check@{ element ->
+                if (!variantCheck(element))
+                    return@check false
+                if (element is CaosDefCommandWord || element is CaosDefCommand) {
+                    LOGGER.info("Is '$name' reference to '${element.text}' in ${myElement.containingFile.name}")
+                    element.text.toUpperCase().let { it == "OBVX" || it == "OVXX" }
+                } else if (element is CaosScriptVarToken) {LOGGER.info("Is '$name' reference to '${element.text}' in ${myElement.containingFile.name}")
+                    varIndex == element.varIndex && element.varGroup.let { it == OBVx || it == OVxx }
+                } else {
+                    false
+                }
+            }
+
+            MVxx -> check@{ element ->
+                if (!variantCheck(element))
+                    return@check false
+                if (element is CaosDefCommandWord || element is CaosDefCommand) {
+                    LOGGER.info("Is '$name' reference to '${element.text}' in ${myElement.containingFile.name}")
+                    element.text.toUpperCase() == "MVXX"
+                } else if (element is CaosScriptVarToken) {
+                    LOGGER.info("Is '$name' reference to '${element.text}' in ${myElement.containingFile.name}")
+                    varIndex == element.varIndex && element.varGroup == MVxx
+                } else {
+                    false
+                }
+            }
+            UNKNOWN -> { _ -> false }
+        }
     }
-*/
+
+    override fun isReferenceTo(element: PsiElement): Boolean {
+        return check(element)
+    }
+
     override fun multiResolve(partial: Boolean): Array<ResolveResult> {
         val key = myElement.varGroup.value
         val variant = myElement.variant
-                ?: return  PsiElementResolveResult.EMPTY_ARRAY
+                ?: return PsiElementResolveResult.createResults(myElement)
         val commands = CaosDefCommandElementsByNameIndex
                 .Instance[key, element.project]
                 .filter {
                     it.isVariant(variant)
                 }
                 .map { it.command }
+                .nullIfEmpty()
+                ?: return PsiElementResolveResult.createResults(myElement)
         return PsiElementResolveResult.createResults(commands)
     }
 }
