a97439577f4bb9ffa33cf695ece279440e5fa492
bedalton <Dsbadal@gmail.com>
2020-12-16 18:41:02 -0800

    add concrete definitions for Creature OWNR scripts

--- src/main/java/com/badahori/creatures/plugins/intellij/agenteering/caos/inspections/CaosScriptRequiresCreatureOwnrInspection.kt
+++ src/main/java/com/badahori/creatures/plugins/intellij/agenteering/caos/inspections/CaosScriptRequiresCreatureOwnrInspection.kt
@@ -1,8 +1,7 @@
 package com.badahori.creatures.plugins.intellij.agenteering.caos.inspections
 
 import com.badahori.creatures.plugins.intellij.agenteering.caos.lang.CaosBundle
-import com.badahori.creatures.plugins.intellij.agenteering.caos.libs.CaosLib
-import com.badahori.creatures.plugins.intellij.agenteering.caos.libs.CaosLibs
+import com.badahori.creatures.plugins.intellij.agenteering.caos.libs.CaosVariant
 import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.CaosScriptCommandElement
 import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.CaosScriptEventScript
 import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.CaosScriptIsCommandToken
@@ -31,22 +30,38 @@ class CaosScriptRequiresCreatureOwnrInspection : LocalInspectionTool() {
         if (!requiresCreatureOwnr)
             return
         element.getParentOfType(CaosScriptEventScript::class.java)?.let { script: CaosScriptEventScript ->
-            if (hasCreatureOwner(script)) {
+            if (script.hasCreatureOwnr) {
                 return
             }
         }
         holder.registerProblem(element, CaosBundle.message("caos.inspections.ownr-inspection.requires-creature-ownr", element.commandString.toUpperCase()))
     }
+}
 
-    private fun hasCreatureOwner(eventScript:CaosScriptEventScript) : Boolean {
-        if (eventScript.family.let { it == 0 || it == 4})
-            return true
-        val variant = eventScript.variant
-            ?: return true
-        val eventNumbers = CaosLibs[variant].valuesList("EventNumbers")
-            ?: return true
-        val eventValue = eventNumbers[eventScript.eventNumber]
-            ?: return false
-        return eventValue.name.toLowerCase().contains("extra")
+/**
+ * Helper function to determine whether an event script has a creature OWNR
+ */
+val CaosScriptEventScript.hasCreatureOwnr:Boolean get() {
+    if (family.let { it == 0 || it == 4})
+        return true
+    val creatureOwnrScripts = variant?.creatureOwnrScripts
+        ?: return true
+    val eventNumber = eventNumber
+    return eventNumber in creatureOwnrScripts
+}
+
+
+/**
+ * Returns a list of Script numbers that have a creature OWNR object
+ */
+val CaosVariant.creatureOwnrScripts:List<Int> get() {
+    return when (this) {
+        CaosVariant.C1 -> c1CreatureOwnrEventNumbers
+        CaosVariant.C2 -> c2CreatureOwnrEventNumbers
+        else -> c2eCreatureOwnrEventNumbers
     }
 }
+
+private val c2eCreatureOwnrEventNumbers = (16..47) + (64..72) + 200
+private val c1CreatureOwnrEventNumbers = (16..47) + (64..72)
+private val c2CreatureOwnrEventNumbers = (16..29) + (32..45)+ (64..72) + 200
