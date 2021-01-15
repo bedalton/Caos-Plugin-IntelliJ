51206de00da6b55ecf5414912aa4a67f063a0717
bedalton <3941960+bedalton@users.noreply.github.com>
2021-01-14 17:32:19 -0800

    Convert CaosScriptLexerAdapter Jave to Kotlin

--- src/main/java/com/badahori/creatures/plugins/intellij/agenteering/caos/lexer/CaosScriptLexerAdapter.java
+++
@@ -1,9 +0,0 @@
-package com.badahori.creatures.plugins.intellij.agenteering.caos.lexer;
-
-import com.intellij.lexer.FlexAdapter;
-
-public class CaosScriptLexerAdapter extends FlexAdapter {
-    public CaosScriptLexerAdapter() {
-            super(new _CaosScriptLexer(false));
-        }
-}
