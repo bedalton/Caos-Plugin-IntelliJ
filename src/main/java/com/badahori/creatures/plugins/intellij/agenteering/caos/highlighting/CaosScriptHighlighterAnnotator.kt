fd787bbea2eae2596491fbe67ff5b8d46276e4f6
bedalton <Dsbadal@gmail.com>
2021-01-18 18:06:16 -0800

    Fix token values not highlighting properly

--- src/main/java/com/badahori/creatures/plugins/intellij/agenteering/caos/highlighting/CaosScriptHighlighterAnnotator.kt
+++ src/main/java/com/badahori/creatures/plugins/intellij/agenteering/caos/highlighting/CaosScriptHighlighterAnnotator.kt
@@ -18,7 +18,7 @@ class CaosScriptHighlighterAnnotator : Annotator {
     override fun annotate(element: PsiElement, annotationHolder: AnnotationHolder) {
         val wrapper = AnnotationHolderWrapper(annotationHolder)
         when {
-            element is CaosScriptIsRvalueKeywordToken || element.parent is CaosScriptIsRvalueKeywordToken -> colorize(element, wrapper, CaosScriptSyntaxHighlighter.RVALUE_TOKEN)
+            (element is CaosScriptIsRvalueKeywordToken || element.parent is CaosScriptIsRvalueKeywordToken) && element.firstChild?.elementType != CaosScriptTypes.CaosScript_TOKEN -> colorize(element, wrapper, CaosScriptSyntaxHighlighter.RVALUE_TOKEN)
             element is CaosScriptIsCommandKeywordToken || (element.parent is CaosScriptIsCommandKeywordToken && element.parent.firstChild != element) -> colorize(element, wrapper, CaosScriptSyntaxHighlighter.COMMAND_TOKEN)
             element is CaosScriptIsLvalueKeywordToken || element.parent is CaosScriptIsLvalueKeywordToken -> colorize(element, wrapper, CaosScriptSyntaxHighlighter.LVALUE_TOKEN)
             element is CaosScriptAnimationString -> colorize(element, wrapper, CaosScriptSyntaxHighlighter.ANIMATION)
@@ -28,6 +28,7 @@ class CaosScriptHighlighterAnnotator : Annotator {
             element is CaosScriptIsSuffixToken -> colorize(element, wrapper, CaosScriptSyntaxHighlighter.SUFFIX_TOKEN)
             element is CaosScriptSubroutineName || element.parent is CaosScriptSubroutineName -> colorize(element, wrapper, CaosScriptSyntaxHighlighter.SUBROUTINE_NAME)
             element.text.toLowerCase() == "inst" -> colorize(element, wrapper, CaosScriptSyntaxHighlighter.KEYWORDS)
+            element is CaosScriptTokenRvalue -> colorize(element, wrapper, CaosScriptSyntaxHighlighter.TOKEN)
             element.elementType == CaosScriptTypes.CaosScript_WORD -> when {
                 element.hasParentOfType(CaosScriptErrorCommand::class.java) -> colorize(element, wrapper, CaosScriptSyntaxHighlighter.ERROR_COMMAND_TOKEN)
                 element.parent is CaosScriptCGsub -> colorize(element, wrapper, CaosScriptSyntaxHighlighter.SUBROUTINE_NAME)
