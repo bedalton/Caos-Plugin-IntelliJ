70519adcd9a2e7736082211d000a289336a45cae
daniel <dsbadal@gmail.com>
2021-03-22 08:04:05 -0700

    Add hasFlag functions for checking flags in Creature's files

--- src/main/java/com/badahori/creatures/plugins/intellij/agenteering/utils/ExtensionMethods.kt
+++ src/main/java/com/badahori/creatures/plugins/intellij/agenteering/utils/ExtensionMethods.kt
@@ -27,3 +27,8 @@ val Any?.canonicalName:String get() = if (this == null) "NULL" else this::class.
 operator fun CaosScriptFile.plus(list:List<CaosScriptFile>) : List<CaosScriptFile> {
     return mutableListOf(this).apply { addAll(list) }
 }
+
+fun Int.hasFlag(flag:Int) : Boolean = this and flag == flag
+
+fun Int.removeFlag(flag:Int) : Int = this and flag.inv()
+fun Int.addFlag(flag:Int) : Int = this or flag
