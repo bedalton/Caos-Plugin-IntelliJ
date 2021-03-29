6c1de104d6320100bf74589988c7ed63688fe230
bedalton <3941960+bedalton@users.noreply.github.com>
2021-03-29 09:04:02 -0700

    Add variable type check method stubs

---
+++ src/main/java/com/badahori/creatures/plugins/intellij/agenteering/caos/deducer/CaosScriptVariableTypeDeducer.kt
@@ -0,0 +1,136 @@
+package com.badahori.creatures.plugins.intellij.agenteering.caos.deducer
+
+import com.badahori.creatures.plugins.intellij.agenteering.caos.indices.CaosScriptNamedGameVarIndex
+import com.badahori.creatures.plugins.intellij.agenteering.caos.lang.module
+import com.badahori.creatures.plugins.intellij.agenteering.caos.libs.CaosScriptNamedGameVarType
+import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.*
+import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.CaosExpressionValueType.*
+import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.impl.containingCaosFile
+import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.types.CaosScriptVarTokenGroup
+import com.badahori.creatures.plugins.intellij.agenteering.utils.orFalse
+import com.badahori.creatures.plugins.intellij.agenteering.utils.startOffset
+import com.intellij.psi.search.GlobalSearchScope
+
+object CaosScriptVariableTypeDeducer {
+
+
+    fun infer(variable:CaosScriptIsVariable, skipNames:MutableList<CaosScriptIsVariable>) : Set<CaosExpressionValueType>  {
+        return emptySet()
+    }
+
+
+}
+
+
+private object NumberedVariableTypeDeducer {
+
+    fun infer(variable: CaosScriptIsVariable, skipNames:MutableList<CaosScriptIsVariable>) : Set<CaosExpressionValueType> {
+        return emptySet()
+    }
+
+}
+
+
+
+private object NamedVariableTypeDeducer {
+
+    fun infer(variable: CaosScriptIsVariable, skipNames:MutableList<CaosScriptIsVariable>) : Set<CaosExpressionValueType> {
+        //TODO()
+        return emptySet()
+    }
+
+}
+
+
+private object GameVariableTypeDeducer {
+    fun infer(variable: CaosScriptNamedGameVar, skipNames:MutableList<CaosScriptIsVariable>) : Set<CaosExpressionValueType> {
+        val module = variable.containingCaosFile?.module
+
+        val scope = if (module != null)
+            GlobalSearchScope.moduleScope(module)
+        else
+            GlobalSearchScope.everythingScope(variable.project)
+        val variableName = variable.name
+        val variableType = variable.varType
+        val thisScriptParent = variable.getParentOfType(CaosScriptScriptElement::class.java)
+        val thisPosition = variable.startOffset
+        var variables = CaosScriptNamedGameVarIndex.instance[variableName, variable.project, scope]
+            .filter { other ->
+                isSimilar(variable, other) && isNotEquivalentToAny(other, skipNames)
+                        && other.parent is CaosScriptLvalue
+                        && other.parent?.parent is CaosScriptIsAssignment
+            }
+        if (variable.varType == CaosScriptNamedGameVarType.NAME || variable.varType == CaosScriptNamedGameVarType.MAME) {
+            variables = variables.filter {
+                // TODO FILTER BY OWNR Object
+                true
+            }
+        }
+        return variables
+            .mapNotNull { other ->
+                val otherVariableParentScript = other.getParentOfType(CaosScriptScriptElement::class.java)
+                // Ensure if they are in the same script, that they are not assigned after this variable
+                if (otherVariableParentScript?.isEquivalentTo(thisScriptParent).orFalse() && thisPosition < other.startOffset)
+                    null
+                else
+                    getAssignedType(other.parent.parent as CaosScriptIsAssignment)
+            }
+            .toSet()
+    }
+}
+
+private fun getAssignedType(other:CaosScriptIsAssignment) : CaosExpressionValueType {
+    return when (other.commandStringUpper) {
+        "SETS" -> STRING
+        "SETA" -> AGENT
+        "SETV" -> DECIMAL
+        "RNDV" -> DECIMAL
+        "NET: UNIK" -> STRING
+        "ADDS" -> STRING
+        "NEGV", "ABSV", "NOTV" -> DECIMAL
+        "CHAR" -> STRING
+        "NEW: GENE" -> INT
+        "ADDV", "SUBV", "MULV", "DIVV", "MODV" -> DECIMAL
+        "ORRV", "ANDV" -> INT
+        else -> INT // TODO Should I throw an error here until I get this sorted
+    }
+}
+
+private fun isNotEquivalentToAny(aVariable: CaosScriptIsVariable, others:List<CaosScriptIsVariable>) : Boolean {
+    return others.none { other ->
+        aVariable.isEquivalentTo(other)
+    }
+}
+
+private fun isSimilar(aVariable: CaosScriptIsVariable, other:CaosScriptIsVariable) : Boolean {
+    return when (aVariable) {
+        is CaosScriptVarToken -> {
+            if (other is CaosScriptVarToken) {
+                if (aVariable.varIndex == other.varIndex) {
+                    val otherIsEventVar = (other.varGroup == CaosScriptVarTokenGroup.VARx || other.varGroup == CaosScriptVarTokenGroup.VAxx)
+                    when (aVariable.varGroup) {
+                        CaosScriptVarTokenGroup.VAxx, CaosScriptVarTokenGroup.VARx -> otherIsEventVar
+                        else -> !otherIsEventVar
+                    }
+                } else {
+                    false
+                }
+            } else {
+                false
+            }
+        }
+        is CaosScriptNamedGameVar -> {
+            if (other is CaosScriptNamedGameVar) {
+                when (val variableType = aVariable.varType) {
+                    CaosScriptNamedGameVarType.NAME, CaosScriptNamedGameVarType.MAME -> aVariable.name == other.name
+                    CaosScriptNamedGameVarType.GAME -> other.varType == variableType && aVariable.name == other.name
+                    CaosScriptNamedGameVarType.EAME -> other.varType == variableType && aVariable.name == other.name
+                    else -> false
+                }
+            } else {
+                false
+            }
+        }
+        else -> false
+    }
+}
---
+++ src/main/java/com/badahori/creatures/plugins/intellij/agenteering/caos/inspections/CaosScriptVariableTypeMismatchInspection.kt
@@ -0,0 +1,36 @@
+package com.badahori.creatures.plugins.intellij.agenteering.caos.inspections
+
+import com.badahori.creatures.plugins.intellij.agenteering.caos.lang.CaosBundle
+import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.CaosScriptIsVariable
+import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.CaosScriptNamedGameVar
+import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.CaosScriptVarToken
+import com.intellij.codeInspection.LocalInspectionTool
+import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.psi.PsiElementVisitor
+
+class CaosScriptVariableTypeMismatchInspection : LocalInspectionTool() {
+
+
+    override fun getDisplayName(): String = "Rndv value is always the same"
+    override fun getGroupDisplayName(): String = CaosBundle.message("caos.intentions.family")
+    override fun getShortName(): String = "RndvIsTheSame"
+
+    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
+        return object : com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.CaosScriptVisitor() {
+            override fun visitVarToken(variable: CaosScriptVarToken) {
+                checkIndexedVariableType(variable)
+            }
+
+            override fun visitNamedGameVar(variable: CaosScriptNamedGameVar) {
+                checkIndexedVariableType(variable)
+            }
+        }
+    }
+
+    private fun checkIndexedVariableType(varToken: CaosScriptIsVariable) {
+        TODO()
+    }
+
+
+
+}
