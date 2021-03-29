28792beda64e4892d704cb7e7c8bd76c88e1b744
bedalton <Dsbadal@gmail.com>
2021-03-29 09:02:33 -0700

    Add command string getters for use in inspections

--- src/main/java/com/badahori/creatures/plugins/intellij/agenteering/caos/psi/api/CaosScriptIsAssignment.kt
+++ src/main/java/com/badahori/creatures/plugins/intellij/agenteering/caos/psi/api/CaosScriptIsAssignment.kt
@@ -2,4 +2,6 @@ package com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api
 
 interface CaosScriptIsAssignment : CaosScriptCompositeElement {
     val lvalue: CaosScriptLvalue?
+    val commandString: String
+    val commandStringUpper: String
 }
