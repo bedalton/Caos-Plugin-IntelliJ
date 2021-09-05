b8f9c77bec14c7371f119e720e829ba6a6504e23
daniel <dsbadal@gmail.com>
2021-09-04 18:35:40 -0700

    Update project settings

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,30 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import com.badahori.creatures.plugins.intellij.agenteering.caos.generator.CaosDefGeneratorTask
+import org.jetbrains.intellij.releaseType
 
 plugins {
-    id("org.jetbrains.intellij") version "0.6.1"
-    kotlin("plugin.serialization") version "1.4.10"
-    kotlin("jvm") version "1.4.10"
+    id("org.jetbrains.intellij") version "1.1.5"
+    kotlin("plugin.serialization") version "1.5.30"
+    kotlin("jvm") version "1.5.30"
 }
 
 group = "com.badahori.creatures.plugins.intellij.agenteering"
-version = "2021.07.25"
+version = "2021.08.31"
 
 
 repositories {
+    mavenLocal()
     mavenCentral()
+    // used to download antlr-kotlin-runtime
+    maven("https://jitpack.io")
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.2")
     implementation("org.apache.commons:commons-imaging:1.0-alpha2")
     testImplementation("junit", "junit", "4.12")
+    implementation("bedalton.creatures:PrayCompiler:0.01.0")
+    implementation("bedalton.creatures:CommonCore:0.01")
 
 }
 sourceSets.main {
@@ -26,6 +32,7 @@ sourceSets.main {
 }
 
 java {
+    releaseType("1.8")
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
 }
@@ -33,29 +40,38 @@ java {
 kotlin {
 
     tasks.withType<KotlinCompile>().all {
-
         kotlinOptions.jvmTarget = "1.8"
+        this.kotlinOptions.apiVersion = "1.4"
+        this.targetCompatibility = "1.8"
+        this.sourceCompatibility = "1.8"
         kotlinOptions.freeCompilerArgs += listOf(
-                "-Xmx4G",
-                "-Xjvm-default=enable",
-                "-Xopt-in=kotlin.RequiresOptIn",
-                "-Xopt-in=kotlin.OptIn",
-                "-Xopt-in=kotlin.ExperimentalMultiplatform",
-                "-Xopt-in=kotlin.ExperimentalUnsignedTypes",
-                "-Xopt-in=kotlin.contracts.ExperimentalContracts",
-                "-Xopt-in=ExperimentalJsExport"
+            "-Xjvm-default=enable",
+            "-Xopt-in=kotlin.RequiresOptIn",
+            "-Xopt-in=kotlin.OptIn",
+            "-Xopt-in=kotlin.ExperimentalMultiplatform",
+            "-Xopt-in=kotlin.ExperimentalUnsignedTypes",
+            "-Xopt-in=kotlin.contracts.ExperimentalContracts",
+            "-Xopt-in=ExperimentalJsExport"
         )
+    }
+
+    sourceSets {
+        all {
+            languageSettings.optIn("kotlin.RequiresOptIn")
+            languageSettings.optIn("kotlin.ExperimentalMultiplatform")
+            //languageSettings.useExperimentalAnnotation("kotlin.contracts.ExperimentalContracts")
+        }
     }
 
 }
 
 // See https://github.com/JetBrains/gradle-intellij-plugin/
 intellij {
-    version = "2019.1"
-    updateSinceUntilBuild = false
-    sameSinceUntilBuild = true
-    sandboxDirectory = "/Users/daniel/Projects/Intellij Sandbox"
-    setPlugins("PsiViewer:191.4212", "com.chrisrm.idea.MaterialThemeUI:3.10.0")
+    version.set("2019.3")
+    updateSinceUntilBuild.set(false)
+    sameSinceUntilBuild.set(true)
+    sandboxDir.set("/Users/daniel/Projects/AppsAndDevelopment/Intellij Plugins/Plugin Sandbox")
+//    plugins.set(listOf("PsiViewer:193-SNAPSHOT", "com.mallowigi.idea:8.0"))
 }
 
 tasks.register<CaosDefGeneratorTask>("generateCaosDef") {
@@ -63,16 +79,26 @@ tasks.register<CaosDefGeneratorTask>("generateCaosDef") {
     this.createFolder = true
     this.generateCaosDef()
 }
-tasks.getByName("runIde") {
+tasks.getByName<org.jetbrains.intellij.tasks.RunIdeTask>("runIde") {
     dependsOn("generateCaosDef")
 }
 tasks.getByName("buildPlugin") {
     dependsOn("generateCaosDef")
 }
 
+tasks.getByName<org.jetbrains.intellij.tasks.RunPluginVerifierTask>("runPluginVerifier") {
+    this.ideVersions.set(
+        listOf(
+            "IU-193.7288.26",
+            "IU-201.8743.12",
+            "IC-212.5080.55"
+        )
+    )
+}
+
 tasks.withType<org.jetbrains.intellij.tasks.RunIdeTask>().all {
-    maxHeapSize = "1g"
-    autoReloadPlugins = true
+    maxHeapSize = "2g"
+    autoReloadPlugins.set(true)
 }
 
 tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml") {
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("plugin.serialization") version "1.4.10"
-    kotlin("jvm") version "1.4.10"
+    kotlin("plugin.serialization") version "1.5.20"
+    kotlin("jvm") version "1.5.20"
 }
 
 repositories {
@@ -19,4 +19,9 @@ sourceSets.main {
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
+}
+kotlin {
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().all {
+        kotlinOptions.jvmTarget = "1.8"
+    }
 }
