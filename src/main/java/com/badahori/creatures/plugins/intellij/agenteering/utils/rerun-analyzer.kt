97c4599e34d26f68928a361c7edc355cf618473b
daniel <dsbadal@gmail.com>
2021-09-04 18:23:13 -0700

    Add convenience function rerunAnalyzer

---
+++ src/main/java/com/badahori/creatures/plugins/intellij/agenteering/utils/rerun-analyzer.kt
@@ -0,0 +1,30 @@
+package com.badahori.creatures.plugins.intellij.agenteering.utils
+
+import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import com.intellij.util.FileContentUtilCore
+
+internal fun rerunAnalyzer(element: PsiElement) {
+    if (!element.isValid)
+        return
+    if (!ApplicationManager.getApplication().isDispatchThread)
+        return
+    val file = element.containingFile
+        ?: return
+    if (!file.isValid)
+        return
+
+    FileContentUtilCore.reparseFiles(file.virtualFile)
+    DaemonCodeAnalyzer.getInstance(file.project).restart(file)
+}
+
+internal fun rerunAnalyzer(file: PsiFile) {
+    if (!file.isValid)
+        return
+    if (!ApplicationManager.getApplication().isDispatchThread)
+        return
+    FileContentUtilCore.reparseFiles(file.virtualFile)
+    DaemonCodeAnalyzer.getInstance(file.project).restart(file)
+}
