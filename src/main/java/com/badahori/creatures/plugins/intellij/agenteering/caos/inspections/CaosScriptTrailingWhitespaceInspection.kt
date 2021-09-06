17085726543704112a885696095276bb3337189a
daniel <dsbadal@gmail.com>
2021-09-06 07:55:46 -0700

    Fix missing error on trailing whitespace

--- src/main/java/com/badahori/creatures/plugins/intellij/agenteering/caos/inspections/CaosScriptTrailingWhitespaceInspection.kt
+++ src/main/java/com/badahori/creatures/plugins/intellij/agenteering/caos/inspections/CaosScriptTrailingWhitespaceInspection.kt
@@ -2,19 +2,14 @@ package com.badahori.creatures.plugins.intellij.agenteering.caos.inspections
 
 import com.badahori.creatures.plugins.intellij.agenteering.att.psi.impl.variant
 import com.badahori.creatures.plugins.intellij.agenteering.caos.fixes.CaosScriptFixTooManySpaces
-import com.badahori.creatures.plugins.intellij.agenteering.caos.fixes.CaosScriptReplaceElementFix
 import com.badahori.creatures.plugins.intellij.agenteering.caos.lang.CaosBundle
 import com.badahori.creatures.plugins.intellij.agenteering.caos.lexer.CaosScriptTypes
-import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.CaosScriptCodeBlockLine
 import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.CaosScriptVisitor
-import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.util.getPreviousNonEmptySibling
-import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.util.next
-import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.util.previous
-import com.badahori.creatures.plugins.intellij.agenteering.utils.orFalse
+import com.badahori.creatures.plugins.intellij.agenteering.utils.next
+import com.badahori.creatures.plugins.intellij.agenteering.utils.previous
 import com.badahori.creatures.plugins.intellij.agenteering.utils.tokenType
 import com.intellij.codeInspection.LocalInspectionTool
 import com.intellij.codeInspection.ProblemsHolder
-import com.intellij.psi.PsiComment
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiElementVisitor
 import com.intellij.psi.TokenType
@@ -34,60 +29,49 @@ class CaosScriptTrailingWhitespaceInspection : LocalInspectionTool() {
             }
         }
     }
+
     /**
      * Annotates spacing errors, mostly multiple whitespaces within a command
      */
     private fun validate(element: PsiElement, holder: ProblemsHolder) {
         val type = element.tokenType
-        if (type != CaosScriptTypes.CaosScript_COMMA && type != TokenType.WHITE_SPACE && type != CaosScriptTypes.CaosScript_NEWLINE)
+        if (type != CaosScriptTypes.CaosScript_COMMA && type != TokenType.WHITE_SPACE && type != CaosScriptTypes.CaosScript_NEWLINE) {
             return
+        }
 
         if (element.variant?.isOld != true)
             return
 
-        if (element is PsiComment)
-            return
-
         // Get this elements text
         val text = element.text
+        val next = element.next
         // Get text before and after this space
-        val nextText = element.next?.text ?: ""
+        val nextText = next?.text ?: ""
         val prevText = element.previous?.text ?: ""
-        if (text.contains(',')) {
-            element.getPreviousNonEmptySibling(false)?.let { previous ->
-                if ((previous as? CaosScriptCodeBlockLine)?.commandCall == null) {
-                    holder.registerProblem(
-                        element,
-                        CaosBundle.message("caos.annotator.syntax-error-annotator.unexpected-comma"),
-                        CaosScriptReplaceElementFix(element, " ", "Replace comma with space", true)
-                    )
-                    return
-                }
-            }
-        }
 
         // Test if previous element is a comma or space
         val previousIsCommaOrSpace = IS_COMMA_OR_SPACE.matches(prevText)
 
         // Is a single space, and not followed by terminating comma or newline
-        if (text.length == 1 && !(COMMA_NEW_LINE_REGEX.matches(nextText) || previousIsCommaOrSpace))
-            return
+        if (text.length == 1 && !(COMMA_NEW_LINE_REGEX.matches(nextText) || previousIsCommaOrSpace)) {
+            if (next != null) {
+                return
+            }
+        }
 
-        val next = element.next
 
         // Find if previously on this line there is a newline
         var prev: PsiElement? = element.previous
-        while(prev != null) {
-            if (prev.tokenType != TokenType.WHITE_SPACE && prev.tokenType != CaosScriptTypes.CaosScript_NEWLINE)
+        while (prev != null) {
+            if (prev.tokenType != TokenType.WHITE_SPACE)
                 break
-            if (!prev.textContains('\n'))
-                prev = prev.previous
-            else
+            if (prev.textContains('\n'))
                 break
+            prev = prev.previous
         }
 
         // Trailing whitespace
-        if (!(element.text.contains('\n') || prev?.textContains('\n').orFalse()) || next == null || prev == null) {
+        if (!(element.text.contains('\n') || prev?.text?.endsWith('\n') == true) || next == null || prev == null) {
             holder.registerProblem(
                 element,
                 CaosBundle.message("caos.annotator.syntax-error-annotator.invalid-trailing-whitespace"),
@@ -95,36 +79,6 @@ class CaosScriptTrailingWhitespaceInspection : LocalInspectionTool() {
             )
             return
         }
-//
-//
-//        // Psi element is empty, denoting a missing space, possible after quote or byte-string or number
-//        if (text.isEmpty()) {
-//            val toMark = TextRange(element.startOffset - 1, next.startOffset + 1)
-//            holder.registerProblem(
-//                element,
-//                toMark,
-//                CaosBundle.message("caos.annotator.syntax-error-annotator.missing-space"),
-//                CaosScriptInsertSpaceFix(next)
-//            )
-//            return
-//        }
-//        // Did check for trailing comma, but this is assumed to be removed before injection
-//        // I think BoBCoB does this and CyberLife CAOS tool strips this as well.
-//        if (nextText.startsWith("\n") || element.node.isDirectlyPrecededByNewline()) {// && variant != CaosVariant.C1) {
-//            return
-//        }
-//        val errorTextRange = element.textRange.let {
-//            if (element.text.length > 1)
-//                TextRange(it.startOffset + 1, it.endOffset)
-//            else
-//                it
-//        }
-//        holder.registerProblem(
-//            element,
-//            errorTextRange,
-//            CaosBundle.message("caos.annotator.syntax-error-annotator.too-many-spaces"),
-//            CaosScriptFixTooManySpaces(element)
-//        )
     }
 
 
