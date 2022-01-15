5e298482a71a9e32ae923204bd7a805573be4645
daniel <dsbadal@gmail.com>
2022-01-15 07:54:53 -0800

    Add extension function to convert variant enum from DS to C3

--- src/main/java/com/badahori/creatures/plugins/intellij/agenteering/utils/ExtensionMethods.kt
+++ src/main/java/com/badahori/creatures/plugins/intellij/agenteering/utils/ExtensionMethods.kt
@@ -1,34 +1,39 @@
 package com.badahori.creatures.plugins.intellij.agenteering.utils
 
 import com.badahori.creatures.plugins.intellij.agenteering.caos.lang.CaosScriptFile
+import com.badahori.creatures.plugins.intellij.agenteering.caos.libs.CaosVariant
 
 
-fun <T> T?.orElse(defaultValue:T) : T {
+fun <T> T?.orElse(defaultValue: T): T {
     return this ?: defaultValue
 }
 
-fun <T> T?.orElse(defaultValue:()->T) : T {
+fun <T> T?.orElse(defaultValue: () -> T): T {
     return this ?: defaultValue()
 }
 
 
-fun Boolean?.orFalse() : Boolean {
+fun Boolean?.orFalse(): Boolean {
     return this ?: false
 }
 
-fun Boolean?.orTrue() : Boolean {
+fun Boolean?.orTrue(): Boolean {
     return this ?: true
 }
 
-val Any?.className:String get() = if (this == null) "NULL" else this::class.java.simpleName
+val Any?.className: String get() = if (this == null) "NULL" else this::class.java.simpleName
 
-val Any?.canonicalName:String get() = if (this == null) "NULL" else this::class.java.canonicalName
+val Any?.canonicalName: String get() = if (this == null) "NULL" else this::class.java.canonicalName
 
-operator fun CaosScriptFile.plus(list:List<CaosScriptFile>) : List<CaosScriptFile> {
+operator fun CaosScriptFile.plus(list: List<CaosScriptFile>): List<CaosScriptFile> {
     return mutableListOf(this).apply { addAll(list) }
 }
 
-infix fun Int.hasFlag(flag:Int) : Boolean = this and flag == flag
+infix fun Int.hasFlag(flag: Int): Boolean = this and flag == flag
 
-fun Int.removeFlag(flag:Int) : Int = this and flag.inv()
-fun Int.addFlag(flag:Int) : Int = this or flag
+fun Int.removeFlag(flag: Int): Int = this and flag.inv()
+fun Int.addFlag(flag: Int): Int = this or flag
+
+
+val CaosVariant.selfOrC3IfDS: CaosVariant
+    get() = if (this == CaosVariant.DS) CaosVariant.C3 else this
