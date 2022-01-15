71edaee9e731a26bd07d39d93f8fd3ee422e2dbc
daniel <dsbadal@gmail.com>
2022-01-15 07:55:17 -0800

    Add convenience method to get items from a JList

--- src/main/java/com/badahori/creatures/plugins/intellij/agenteering/utils/combo-box.kt
+++ src/main/java/com/badahori/creatures/plugins/intellij/agenteering/utils/combo-box.kt
@@ -2,6 +2,7 @@ package com.badahori.creatures.plugins.intellij.agenteering.utils
 
 import javax.swing.ComboBoxModel
 import javax.swing.JComboBox
+import javax.swing.JList
 
 val <T> JComboBox<T>.items: List<T>? get() {
     val model = model
@@ -28,4 +29,12 @@ inline fun <T> JComboBox<T>.selectItem(evenIfNull: Boolean = true, check: (item:
         return null
     selectedItem = item
     return item
+}
+
+val <T> JList<T>.items: List<T> get() {
+    val model = model
+    val size = model.size
+    return (0 until size).map { i ->
+        model.getElementAt(i)
+    }
 }
