552c4643335ec5d3231216ad5069a4ac3f30f072
daniel <dsbadal@gmail.com>
2022-01-17 18:06:44 -0800

    Increment stub versions

--- src/main/java/com/badahori/creatures/plugins/intellij/agenteering/caos/stubs/CaosScriptStubVersions.kt
+++ src/main/java/com/badahori/creatures/plugins/intellij/agenteering/caos/stubs/CaosScriptStubVersions.kt
@@ -1,5 +1,5 @@
 package com.badahori.creatures.plugins.intellij.agenteering.caos.stubs
 
 private const val MINOR_VERSION:Int = 0
-private const val MAJOR_VERSION:Int = 20
+private const val MAJOR_VERSION:Int = 21
 const val CAOS_SCRIPT_STUB_VERSION = MAJOR_VERSION + MINOR_VERSION;
