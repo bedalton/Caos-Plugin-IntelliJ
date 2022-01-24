7e7c8509dfb322150b683837bf6c0b209af4a8d5
daniel <dsbadal@gmail.com>
2022-01-24 15:20:59 -0800

    Allow renaming named variables

--- src/main/java/com/badahori/creatures/plugins/intellij/agenteering/caos/references/CaosScriptVetoRenameCondition.kt
+++ src/main/java/com/badahori/creatures/plugins/intellij/agenteering/caos/references/CaosScriptVetoRenameCondition.kt
@@ -3,17 +3,27 @@ package com.badahori.creatures.plugins.intellij.agenteering.caos.references
 import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.*
 import com.intellij.openapi.util.Condition
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
 
 class CaosScriptVetoRenameCondition : Condition<PsiElement> {
     override fun value(element: PsiElement?): Boolean {
         if (element == null)
             return true
-        if (element is CaosScriptSubroutineName || element is CaosScriptSubroutine)
+        if (element is PsiFile) {
             return false
-        if (element is CaosScriptQuoteStringLiteral || element is CaosScriptStringText)
+        }
+        if (element !is CaosScriptCompositeElement) {
             return false
-        if (element is CaosScriptNamedGameVar)
+        }
+        if (element is CaosScriptSubroutineName || element is CaosScriptSubroutine) {
             return false
+        }
+        if (element is CaosScriptQuoteStringLiteral || element is CaosScriptStringText) {
+            return false
+        }
+        if (element is CaosScriptNamedGameVar) {
+            return false
+        }
         return true
     }
 }
