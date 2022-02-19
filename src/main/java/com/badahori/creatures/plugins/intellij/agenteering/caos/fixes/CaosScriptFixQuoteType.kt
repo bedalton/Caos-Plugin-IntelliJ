593389511fee137a92c00c0065bbca7f3ef75cfc
daniel <dsbadal@gmail.com>
2022-02-18 16:36:34 -0800

    Fix error causing quote fix to replace all text with an open bracket

--- src/main/java/com/badahori/creatures/plugins/intellij/agenteering/caos/fixes/CaosScriptFixQuoteType.kt
+++ src/main/java/com/badahori/creatures/plugins/intellij/agenteering/caos/fixes/CaosScriptFixQuoteType.kt
@@ -1,6 +1,7 @@
     package com.badahori.creatures.plugins.intellij.agenteering.caos.fixes
 
 import com.badahori.creatures.plugins.intellij.agenteering.bundles.general.CAOSScript
+import com.badahori.creatures.plugins.intellij.agenteering.caos.formatting.CaosScriptBlock
 import com.intellij.codeInspection.IntentionAndQuickFixAction
 import com.intellij.codeInspection.ProblemDescriptor
 import com.intellij.openapi.editor.Editor
@@ -9,12 +10,15 @@ import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.intellij.psi.SmartPointerManager
 import com.badahori.creatures.plugins.intellij.agenteering.caos.lang.CaosBundle
+import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.CaosScriptCodeBlock
 import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.CaosScriptRvalue
 import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.CaosScriptStringLike
 import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.util.CaosScriptPsiElementFactory
-import com.badahori.creatures.plugins.intellij.agenteering.utils.getSelfOrParentOfType
+import com.badahori.creatures.plugins.intellij.agenteering.utils.*
+import com.intellij.openapi.util.TextRange
+import com.intellij.psi.text.BlockSupport
 
-class CaosScriptFixQuoteType(element:PsiElement, private val quoteStart:Char, private val quoteEnd:Char = quoteStart) : IntentionAndQuickFixAction() {
+    class CaosScriptFixQuoteType(element:PsiElement, private val quoteStart:Char, private val quoteEnd:Char = quoteStart) : IntentionAndQuickFixAction() {
     private val pointer = SmartPointerManager.createPointer(element)
 
     override fun getName(): String = CaosBundle.message("caos.fixes.fix-quote-type")
@@ -34,12 +38,14 @@ class CaosScriptFixQuoteType(element:PsiElement, private val quoteStart:Char, pr
     private fun applyFix(project:Project, element:PsiElement) {
         val expression = element.getSelfOrParentOfType(CaosScriptStringLike::class.java)
                 ?: return
-        val newElement = CaosScriptPsiElementFactory.createStringRValue(
-                project,
-                expression.stringValue,
-                quoteStart,
-                quoteEnd
-        )
-        expression.getSelfOrParentOfType(CaosScriptRvalue::class.java)?.replace(newElement)
+        val document = element.document
+            ?: return
+        val startOffset = element.startOffset
+        val endOffset = element.endOffset
+        val text = expression.stringValue
+        val replaceWith = "$quoteStart$text$quoteEnd"
+        runUndoTransparentWriteAction {
+            EditorUtil.replaceText(document, TextRange(startOffset, endOffset), replaceWith)
+        }
     }
 }
