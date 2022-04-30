33964fcd4825d31f0d35a591e203f3c108ddf66a
daniel <dsbadal@gmail.com>
2022-04-29 18:57:10 -0700

    Add extension methods to help with text range and byte sequences

--- src/main/java/com/badahori/creatures/plugins/intellij/agenteering/utils/ExtensionMethods.kt
+++ src/main/java/com/badahori/creatures/plugins/intellij/agenteering/utils/ExtensionMethods.kt
@@ -3,6 +3,7 @@ package com.badahori.creatures.plugins.intellij.agenteering.utils
 import com.badahori.creatures.plugins.intellij.agenteering.caos.lang.CaosScriptFile
 import com.badahori.creatures.plugins.intellij.agenteering.caos.libs.CaosVariant
 import com.intellij.openapi.util.TextRange
+import com.intellij.openapi.util.io.ByteSequence
 
 
 fun <T> T?.orElse(defaultValue: T): T {
@@ -40,6 +41,29 @@ val CaosVariant.selfOrC3IfDS: CaosVariant
     get() = if (this == CaosVariant.DS) CaosVariant.C3 else this
 
 
-operator fun TextRange.minus(range:TextRange): TextRange {
-    return TextRange(startOffset - range.startOffset, endOffset - range.endOffset)
+internal operator fun TextRange.minus(range:TextRange): TextRange? {
+    val startOffset = startOffset - range.startOffset
+    val endOffset = endOffset - range.endOffset
+    return if (startOffset > endOffset)
+        null
+    else
+        TextRange(startOffset, endOffset)
+}
+
+internal fun TextRange.isNeg(): Boolean {
+    return startOffset < 0 || endOffset < 0
+}
+
+val Byte.char:Char get() = this.toInt().toChar()
+
+fun ByteSequence.charAt(i: Int): Char = this.byteAt(i).toInt().toChar()
+
+fun ByteSequence.charAtOrNull(i: Int): Char? = try {
+    if (length() > i) {
+        this.byteAt(i).toInt().toChar()
+    } else {
+        null
+    }
+} catch (e: Exception) {
+    null
 }
