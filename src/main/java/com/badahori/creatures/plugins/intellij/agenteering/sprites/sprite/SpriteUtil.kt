bcb072b111e903c736bc707fdf8396a89546086b
daniel <dsbadal@gmail.com>
2022-05-04 10:45:57 -0700

    Remove unnecessary imports

--- src/main/java/com/badahori/creatures/plugins/intellij/agenteering/sprites/sprite/SpriteUtil.kt
+++ src/main/java/com/badahori/creatures/plugins/intellij/agenteering/sprites/sprite/SpriteUtil.kt
@@ -1,9 +1,5 @@
 package com.badahori.creatures.plugins.intellij.agenteering.sprites.sprite
 
-import com.badahori.creatures.plugins.intellij.agenteering.utils.FileNameUtils
-import icons.CaosScriptIcons
-import javax.swing.Icon
-
 object SpriteUtil {
 
 
