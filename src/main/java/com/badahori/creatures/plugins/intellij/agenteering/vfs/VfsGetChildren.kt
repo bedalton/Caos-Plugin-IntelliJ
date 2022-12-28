c61a5a18f787d9cc1bb5420dd2fdb1e700be9ce1
daniel <dsbadal@gmail.com>
2022-12-28 08:13:22 -0800

    Add VirtualFile child checks with IntelliJ updated API

--- src/main/java/com/badahori/creatures/plugins/intellij/agenteering/vfs/VfsGetChildren.kt
+++ src/main/java/com/badahori/creatures/plugins/intellij/agenteering/vfs/VfsGetChildren.kt
@@ -26,6 +26,9 @@ internal fun VirtualFile.collectChildren(filter: (VirtualFile) -> Boolean = { tr
     return out
 }
 
+/**
+ * Collects children in virtual file, transforming them as needed
+ */
 internal fun <T> VirtualFile.collectChildrenAs(transform: (VirtualFile) -> T? ): List<T> {
     if (!this.isDirectory) {
         return emptyList()
@@ -43,4 +46,99 @@ internal fun <T> VirtualFile.collectChildrenAs(transform: (VirtualFile) -> T? ):
         }
     })
     return out
+}
+
+/**
+ * Collects children in virtual file, transforming them as needed
+ */
+internal fun <T> VirtualFile.collectChildrenAsNullable(transform: (VirtualFile) -> T? ): List<T?> {
+    if (!this.isDirectory) {
+        return emptyList()
+    }
+    val out = mutableListOf<T?>()
+    VfsUtilCore.visitChildrenRecursively(this, object : VirtualFileVisitor<Void?>() {
+        // good
+        override fun visitFile(file: VirtualFile): Boolean {
+            if (!file.isDirectory) {
+                out.add(transform(file))
+            }
+            return true
+        }
+    })
+    return out
+}
+
+
+/**
+ * Checks that all children within a virtual file directory match the predicate
+ */
+internal fun VirtualFile.allChildrenMatch(check: (VirtualFile) -> Boolean): Boolean {
+    if (!this.isDirectory) {
+        return false
+    }
+    var okay = true
+
+    // Loop through children
+    // Use this helper method to prevent infinite recursion
+    VfsUtilCore.visitChildrenRecursively(this, object : VirtualFileVisitor<Void?>() {
+        // Check each file to see if it matches predicate
+        override fun visitFile(file: VirtualFile): Boolean {
+            if (!file.isDirectory) {
+                if (!check(file)) {
+                    okay = false
+                }
+            }
+            // As long as all files match, continue searching through children
+            return okay
+        }
+    })
+    return okay
+}
+
+/**
+ * Checks that any child within a virtual file directory matches the predicate
+ */
+internal fun VirtualFile.anyChildMatches(check: (VirtualFile) -> Boolean): Boolean {
+    if (!this.isDirectory) {
+        return false
+    }
+    var hasMatch = false
+    VfsUtilCore.visitChildrenRecursively(this, object : VirtualFileVisitor<Void?>() {
+        // good
+        override fun visitFile(file: VirtualFile): Boolean {
+            if (!file.isDirectory) {
+                if (check(file)) {
+                    hasMatch = true
+                }
+            }
+            return !hasMatch
+        }
+    })
+    return hasMatch
+}
+
+/**
+ * Checks that no child in virtual file matches predicate
+ */
+internal fun VirtualFile.noChildMatches(check: (VirtualFile) -> Boolean): Boolean {
+    if (!this.isDirectory) {
+        return false
+    }
+    var matchesNone = true
+    VfsUtilCore.visitChildrenRecursively(this, object : VirtualFileVisitor<Void?>() {
+
+        // Checks every file until it finds a match
+        // If a match is found:
+        // - set match to true
+        // - stop searching
+        override fun visitFile(file: VirtualFile): Boolean {
+            if (!file.isDirectory) {
+                if (check(file)) {
+                    matchesNone = false
+                }
+            }
+            return !matchesNone
+        }
+    })
+    return matchesNone
 }
