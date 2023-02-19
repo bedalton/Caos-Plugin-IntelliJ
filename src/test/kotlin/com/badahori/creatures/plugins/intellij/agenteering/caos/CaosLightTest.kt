b1d33e782a3c3be4a141d846f20b03cbc9dfaae6
daniel <dsbadal@gmail.com>
2023-02-19 09:21:45 -0800

    Prepare for plugin testing
    To allow light tests, the reference to a java platform module was removed

---
+++ src/test/kotlin/com/badahori/creatures/plugins/intellij/agenteering/caos/CaosLightTest.kt
@@ -0,0 +1,16 @@
+package com.badahori.creatures.plugins.intellij.agenteering.caos
+
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
+
+class CaosCompletionTest: BasePlatformTestCase() {
+
+
+    fun testSubroutineNameCompletion() {
+//        myFixture.configureByFiles("")
+    }
+
+
+    override fun getTestDataPath(): String {
+        return "src/main/testData"
+    }
+}
