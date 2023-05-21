a6671fd9cd42d932226c1165ff358dcdc68f3e20
bedalton <Dsbadal@gmail.com>
2023-05-21 08:09:36 -0700

    Allow renaming tokens and catalogue names

--- src/main/java/com/badahori/creatures/plugins/intellij/agenteering/caos/references/CaosScriptVetoRenameCondition.kt
+++ src/main/java/com/badahori/creatures/plugins/intellij/agenteering/caos/references/CaosScriptVetoRenameCondition.kt
@@ -1,14 +1,18 @@
 package com.badahori.creatures.plugins.intellij.agenteering.caos.references
 
 import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.*
+import com.badahori.creatures.plugins.intellij.agenteering.catalogue.psi.api.CatalogueItemName
 import com.intellij.openapi.util.Condition
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 
 class CaosScriptVetoRenameCondition : Condition<PsiElement> {
+    @Suppress("RedundantIf")
     override fun value(element: PsiElement?): Boolean {
-        if (element == null)
+        if (element == null) {
             return true
+        }
+
         if (element is PsiFile) {
             return false
         }
@@ -24,6 +28,15 @@ class CaosScriptVetoRenameCondition : Condition<PsiElement> {
         if (element is CaosScriptNamedGameVar) {
             return false
         }
+
+        if (element is CaosScriptToken) {
+            return false
+        }
+
+        if (element is CatalogueItemName) {
+            return false
+        }
+
         return true
     }
 }
