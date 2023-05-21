618500a80a8882b0f19a6436f750fc65a4cd12be
bedalton <Dsbadal@gmail.com>
2023-05-21 07:39:14 -0700

    Index rvalue tokens
    Begin indexing rvalue tokens to prepare for handling rename of associated files

--- src/main/java/com/badahori/creatures/plugins/intellij/agenteering/caos/stubs/types/CaosScriptTokenRValueStubType.kt
+++ src/main/java/com/badahori/creatures/plugins/intellij/agenteering/caos/stubs/types/CaosScriptTokenRValueStubType.kt
@@ -1,5 +1,7 @@
 package com.badahori.creatures.plugins.intellij.agenteering.caos.stubs.types
 
+import com.badahori.creatures.plugins.intellij.agenteering.caos.indices.CaosScriptIndexService
+import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.CaosScriptTokenRvalue
 import com.intellij.psi.stubs.StubElement
 import com.intellij.psi.stubs.StubInputStream
 import com.intellij.psi.stubs.StubOutputStream
@@ -9,6 +11,9 @@ import com.badahori.creatures.plugins.intellij.agenteering.caos.stubs.api.CaosSc
 import com.badahori.creatures.plugins.intellij.agenteering.caos.stubs.api.StringStubKind
 import com.badahori.creatures.plugins.intellij.agenteering.caos.stubs.impl.CaosScriptTokenRValueStubImpl
 import com.badahori.creatures.plugins.intellij.agenteering.caos.utils.readNameAsString
+import com.intellij.lang.ASTNode
+import com.intellij.openapi.components.ServiceManager
+import com.intellij.psi.stubs.IndexSink
 
 class CaosScriptTokenRValueStubType(debugName:String) : CaosScriptStubElementType<CaosScriptTokenRValueStub, CaosScriptTokenRvalueImpl>(debugName) {
     override fun createPsi(parent: CaosScriptTokenRValueStub): CaosScriptTokenRvalueImpl {
@@ -28,6 +33,19 @@ class CaosScriptTokenRValueStubType(debugName:String) : CaosScriptStubElementTyp
 
     override fun createStub(element: CaosScriptTokenRvalueImpl, parent: StubElement<*>?): CaosScriptTokenRValueStub {
         return CaosScriptTokenRValueStubImpl(parent, element.token?.text, element.stringStubKind)
+    }
+
+    override fun shouldCreateStub(node: ASTNode?): Boolean {
+        if (node?.text.isNullOrBlank()) {
+            return false
+        }
+        val psi = node?.psi
+            ?: return false
+        return StringStubKind.fromPsiElement(psi)?.isFile == true
+    }
+
+    override fun indexStub(stub: CaosScriptTokenRValueStub, indexSink: IndexSink) {
+        ServiceManager.getService(CaosScriptIndexService::class.java).indexToken(stub, indexSink)
     }
 
 }
