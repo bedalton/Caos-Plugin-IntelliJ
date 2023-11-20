8a3ba7cc89bc132287ce0165a2592822f65a3bbd
daniel <dsbadal@gmail.com>
2023-11-20 08:59:51 -0800

    Add intellij LOG constants

--- src/main/java/com/badahori/creatures/plugins/intellij/agenteering/utils/utils.kt
+++ src/main/java/com/badahori/creatures/plugins/intellij/agenteering/utils/utils.kt
@@ -3,6 +3,9 @@ package com.badahori.creatures.plugins.intellij.agenteering.utils
 import java.util.*
 
 
+internal const val INTELLIJ_LOG = "INTELLIJ_LOG"
+internal const val ENABLE_INTELLIJ_LOGS = true
+
 private val random: Random = Random()
 
 fun rand(min: Int, max: Int): Int {
