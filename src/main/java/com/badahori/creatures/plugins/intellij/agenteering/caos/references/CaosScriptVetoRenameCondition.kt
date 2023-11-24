76959478cf2fd7fbc7bc85a5652f2d6498b01423
daniel <dsbadal@gmail.com>
2023-11-24 12:51:35 -0800

    Veto invalid elements

--- src/main/java/com/badahori/creatures/plugins/intellij/agenteering/caos/references/CaosScriptVetoRenameCondition.kt
+++ src/main/java/com/badahori/creatures/plugins/intellij/agenteering/caos/references/CaosScriptVetoRenameCondition.kt
@@ -1,11 +1,8 @@
 package com.badahori.creatures.plugins.intellij.agenteering.caos.references
 
-import com.badahori.creatures.plugins.intellij.agenteering.caos.libs.CaosVariant
 import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.api.*
-import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.impl.variant
-import com.badahori.creatures.plugins.intellij.agenteering.caos.psi.util.parentParameter
 import com.badahori.creatures.plugins.intellij.agenteering.catalogue.psi.api.CatalogueItemName
-import com.badahori.creatures.plugins.intellij.agenteering.utils.hasParentOfType
+import com.badahori.creatures.plugins.intellij.agenteering.utils.isInvalid
 import com.intellij.openapi.util.Condition
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
@@ -17,36 +14,30 @@ class CaosScriptVetoRenameCondition : Condition<PsiElement> {
      */
     @Suppress("RedundantIf")
     override fun value(element: PsiElement?): Boolean {
-        if (element == null) {
+        if (element == null || element.isInvalid) {
             return true
         }
 
         if (element is PsiFile) {
             return false
         }
+
         if (element !is CaosScriptCompositeElement) {
             return false
         }
+
         if (element is CaosScriptSubroutineName || element is CaosScriptSubroutine) {
             return false
         }
+
         if (element is CaosScriptQuoteStringLiteral) {
-//            if (element.hasParentOfType(CaosScriptCaos2Block::class.java)) {
-//                return true
-//            }
-//            val argument = element.getParentOfType(CaosScriptArgument::class.java)
-//                ?: return false
-//            val parameter = argument.parentParameter
-//                ?: return false
-//            val variant = element.variant
-//            val valuesList = parameter.valuesList[variant ?: CaosVariant.DS]
-//                ?: return false
-//            return valuesList.name.startsWith("File.")
             return false
         }
+
         if (element is CaosScriptStringText) {
             return false
         }
+
         if (element is CaosScriptNamedGameVar) {
             return false
         }
