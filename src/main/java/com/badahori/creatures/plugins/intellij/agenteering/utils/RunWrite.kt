e165426d524d52e1a458769c454eb46795a8ae75
daniel <3941960+bedalton@users.noreply.github.com>
2025-01-22 10:08:03 -0800

    Remove old IDEA backwards compat overrides

--- src/main/java/com/badahori/creatures/plugins/intellij/agenteering/utils/RunWrite.kt
+++ src/main/java/com/badahori/creatures/plugins/intellij/agenteering/utils/RunWrite.kt
@@ -31,58 +31,7 @@ internal inline fun <T> computeDelegated(executor: (setter: (T) -> Unit) -> Unit
     return resultRef as T
 }
 
-@Deprecated(
-    "Use IntelliJ official invokeLater",
-    replaceWith = ReplaceWith(
-        "com.intellij.openapi.application.invokeLater(runnable)",
-        imports = ["com.intellij.openapi.application.invokeLater"]
-    )
-)
-inline fun invokeLater(crossinline runnable: () -> Unit) {
-    com.intellij.openapi.application.invokeLater(null, runnable)
-}
-
-@Deprecated(
-    "Use IntelliJ official invokeLater",
-    replaceWith = ReplaceWith(
-        "com.intellij.openapi.application.invokeLater(modality, runnable)",
-        imports = ["com.intellij.openapi.application.invokeLater"]
-    )
-)
-fun invokeLater(modality: ModalityState, runnable: () -> Unit) {
-    com.intellij.openapi.application.invokeLater(modality, runnable)
-}
-
-
 inline fun <T> executeOnPooledThread(crossinline callable: () -> T): Future<T> {
-    val application = ApplicationManager.getApplication();
+    val application = ApplicationManager.getApplication()
     return application.executeOnPooledThread(Callable { callable() })
-}
-
-fun executeOnPooledThread(runnable: Runnable): Future<*> {
-    val application = ApplicationManager.getApplication();
-    return application.executeOnPooledThread(runnable)
-}
-
-@Deprecated(
-    "Use IntelliJ official invokeLater",
-    replaceWith = ReplaceWith(
-        "com.intellij.openapi.application.runWriteAction(runnable)",
-        imports = ["com.intellij.openapi.application.runWriteAction"]
-    )
-)
-inline fun <T> runWriteAction(crossinline runnable: () -> T): T {
-    return com.intellij.openapi.application.runWriteAction(runnable)
-}
-
-
-@Deprecated(
-    "Use IntelliJ official runUndoTransparentAction(runnable)",
-    replaceWith = ReplaceWith(
-        "com.intellij.openapi.application.runUndoTransparentWriteAction(runnable)",
-        imports = ["com.intellij.openapi.application.runUndoTransparentWriteAction"]
-    )
-)
-inline fun <T> runUndoTransparentWriteAction(crossinline runnable: () -> T): T {
-    return com.intellij.openapi.application.runUndoTransparentWriteAction(runnable)
 }
