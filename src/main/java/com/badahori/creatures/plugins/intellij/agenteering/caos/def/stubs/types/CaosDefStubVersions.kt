fced4361d61d199e28be5f178f7f56d0511e2b59
daniel <3941960+bedalton@users.noreply.github.com>
2025-01-23 08:46:46 -0800

    Fix CAOS def with modulus in values list values

--- src/main/java/com/badahori/creatures/plugins/intellij/agenteering/caos/def/stubs/types/CaosDefStubVersions.kt
+++ src/main/java/com/badahori/creatures/plugins/intellij/agenteering/caos/def/stubs/types/CaosDefStubVersions.kt
@@ -3,6 +3,6 @@ package com.badahori.creatures.plugins.intellij.agenteering.caos.def.stubs.types
 object CaosDefStubVersions
 {
     private const val MINOR_VERSION = 0
-    private const val MAJOR_VERSION = 3
+    private const val MAJOR_VERSION = 6
     const val STUB_VERSION =  MAJOR_VERSION + MINOR_VERSION
 }
