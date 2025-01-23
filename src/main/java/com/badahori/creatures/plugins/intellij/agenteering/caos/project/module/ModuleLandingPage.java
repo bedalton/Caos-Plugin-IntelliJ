e7819707ee789d24d47f29498ef3779644bd8d5c
daniel <3941960+bedalton@users.noreply.github.com>
2025-01-23 08:54:52 -0800

    Fix deprecated SVG icon in module page

--- src/main/java/com/badahori/creatures/plugins/intellij/agenteering/caos/project/module/ModuleLandingPage.java
+++ src/main/java/com/badahori/creatures/plugins/intellij/agenteering/caos/project/module/ModuleLandingPage.java
@@ -1,12 +1,17 @@
 package com.badahori.creatures.plugins.intellij.agenteering.caos.project.module;
 
-import com.au.id.mcc.adapted.swing.SVGIcon;
 import com.intellij.openapi.progress.ProcessCanceledException;
 import com.intellij.util.ImageLoader;
 import com.intellij.util.ui.JBImageIcon;
+import org.jetbrains.annotations.NotNull;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BaseMultiResolutionImage;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.util.concurrent.CancellationException;
 import java.util.logging.Logger;
@@ -72,12 +77,12 @@ public class ModuleLandingPage {
 
     private Icon createEggIconSVG(final int width, final int height) {
         try {
-            final URL resource = this.getClass().getResource("/icons/_White-Egg.svg");
-            final String path = resource != null ? resource.toExternalForm() : null;
-            if (path == null) {
-                return null;
-            }
-            return new SVGIcon(path, width, height);
+
+            final BufferedImage image1x = readIconImage("/icons/_White-Egg.png");
+            final BufferedImage image2x = readIconImage("/icons/_White-Egg@2x.png");
+            final BufferedImage image3x = readIconImage("/icons/_White-Egg@3x.png");
+            final Image image = new BaseMultiResolutionImage(new BufferedImage[] { image1x, image2x, image3x });
+            return new JBImageIcon(image);
         } catch (Exception e) {
             if (e instanceof ProcessCanceledException) {
                 throw (ProcessCanceledException)e;
@@ -88,6 +93,16 @@ public class ModuleLandingPage {
             Logger.getAnonymousLogger().severe("Error loading egg image from resource");
             e.printStackTrace();
             return null;
+        }
+    }
+
+    @NotNull
+    private BufferedImage readIconImage(final String name) throws IOException {
+        try(final InputStream stream = this.getClass().getResourceAsStream(name)) {
+            if (stream == null) {
+                throw new IOException("Failed to get stream for icon: " + name);
+            }
+            return ImageIO.read(stream);
         }
     }
 }
